1019a9e3a809109a18ba62acf004b11d39862f3e
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:26:10 +0300

    Update SnooperBaseActivity.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/activity/SnooperBaseActivity.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/activity/SnooperBaseActivity.kt
@@ -6,59 +6,113 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
-import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
-
+import com.prateekj.snooper.AndroidSnooper.Companion.ACTION_END_SNOOPER_FLOW
 import com.prateekj.snooper.infra.AppPermissionChecker
 
-import com.prateekj.snooper.AndroidSnooper.Companion.ACTION_END_SNOOPER_FLOW
+abstract class SnooperBaseActivity :
+    AppCompatActivity() {
 
-abstract class SnooperBaseActivity : AppCompatActivity() {
+    private lateinit var receiver:
+            BroadcastReceiver
 
-  private lateinit var receiver: BroadcastReceiver
-  protected lateinit var appPermissionChecker: AppPermissionChecker
+    protected lateinit var appPermissionChecker:
+            AppPermissionChecker
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    receiver = finishActivityReceiver(this)
-    appPermissionChecker = AppPermissionChecker(this)
-  }
+    override fun onCreate(
+        savedInstanceState: Bundle?
+    ) {
 
-  override fun onResume() {
-    super.onResume()
-    LocalBroadcastManager.getInstance(this)
-      .registerReceiver(receiver, IntentFilter(ACTION_END_SNOOPER_FLOW))
-  }
+        super.onCreate(savedInstanceState)
 
-  override fun onPause() {
-    super.onPause()
-    LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver)
-  }
+        receiver =
+            createFinishActivityReceiver(
+                this
+            )
 
-  override fun onOptionsItemSelected(item: MenuItem): Boolean {
-    if (item.itemId == android.R.id.home) {
-      onBackPressed()
-      return true
+        appPermissionChecker =
+            AppPermissionChecker(this)
     }
-    return super.onOptionsItemSelected(item)
-  }
 
-  override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<String>,
-    grantResults: IntArray
-  ) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    appPermissionChecker.handlePermissionResult(requestCode, permissions, grantResults)
-  }
+    override fun onResume() {
 
-  private fun finishActivityReceiver(activity: Activity): BroadcastReceiver {
-    return object : BroadcastReceiver() {
-      override fun onReceive(context: Context, intent: Intent) {
-        ActivityCompat.finishAffinity(activity)
-      }
+        super.onResume()
+
+        LocalBroadcastManager
+            .getInstance(this)
+            .registerReceiver(
+                receiver,
+                IntentFilter(
+                    ACTION_END_SNOOPER_FLOW
+                )
+            )
     }
-  }
+
+    override fun onPause() {
+
+        LocalBroadcastManager
+            .getInstance(this)
+            .unregisterReceiver(receiver)
+
+        super.onPause()
+    }
+
+    override fun onOptionsItemSelected(
+        item: MenuItem
+    ): Boolean {
+
+        return when (item.itemId) {
+
+            android.R.id.home -> {
+
+                onBackPressedDispatcher.onBackPressed()
+
+                true
+            }
+
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+
+        super.onRequestPermissionsResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
+
+        appPermissionChecker.handlePermissionResult(
+            requestCode,
+            permissions,
+            grantResults
+        )
+    }
+
+    private fun createFinishActivityReceiver(
+        activity: Activity
+    ): BroadcastReceiver {
+
+        return object : BroadcastReceiver() {
+
+            override fun onReceive(
+                context: Context?,
+                intent: Intent?
+            ) {
+
+                ActivityCompat.finishAffinity(
+                    activity
+                )
+            }
+        }
+    }
 }
