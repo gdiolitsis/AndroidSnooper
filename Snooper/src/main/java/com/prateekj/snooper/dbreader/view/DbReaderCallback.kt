86f18326e679422b312457e6bbd7286cedc47278
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 01:29:06 +0300

    Update DbReaderCallback.kt

--- Snooper/src/main/java/com/prateekj/snooper/dbreader/view/DbReaderCallback.kt
+++ Snooper/src/main/java/com/prateekj/snooper/dbreader/view/DbReaderCallback.kt
@@ -1,9 +1,12 @@
 package com.prateekj.snooper.dbreader.view
 
-
 import com.prateekj.snooper.dbreader.model.Database
 
 interface DbReaderCallback {
-  fun onDbFetchStarted()
-  fun onApplicationDbFetchCompleted(databases: List<Database>)
+
+    fun onDbFetchStarted()
+
+    fun onApplicationDbFetchCompleted(
+        databases: List<Database>
+    )
 }
