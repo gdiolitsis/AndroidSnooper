739d368dd0b778cc89e98a8a2945a62db7d1c82d
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 11:57:10 +0300

    Update XmlFormatterTest.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/formatter/XmlFormatterTest.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/formatter/XmlFormatterTest.kt
@@ -1,30 +1,53 @@
 package com.prateekj.snooper.formatter
 
-import org.junit.Test
-
 import com.prateekj.snooper.utils.TestUtilities.readFrom
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
+import org.junit.Test
 
 class XmlFormatterTest {
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldReturnFormattedXmlObject() {
-    val formatter = XmlFormatter()
-    val formattedResponse = formatter.format(readFrom("person_details_raw_response.xml"))
-    val expectedResponse = readFrom("person_details_formatted_response.xml")
+    @Test
+    @Throws(Exception::class)
+    fun shouldReturnFormattedXmlObject() {
 
-    assertThat(formattedResponse, `is`<String>(expectedResponse))
-  }
+        val formatter =
+            XmlFormatter()
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldReturnSameXmlWhenExceptionOccurs() {
-    val formatter = XmlFormatter()
-    val xml = "<invalid>1</tags>"
-    val formattedResponse = formatter.format(xml)
+        val formattedResponse =
+            formatter.format(
+                readFrom(
+                    "person_details_raw_response.xml"
+                )
+            )
 
-    assertThat(formattedResponse, `is`(xml))
-  }
+        val expectedResponse =
+            readFrom(
+                "person_details_formatted_response.xml"
+            )
+
+        assertThat(
+            formattedResponse,
+            `is`(expectedResponse)
+        )
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun shouldReturnSameXmlWhenExceptionOccurs() {
+
+        val formatter =
+            XmlFormatter()
+
+        val xml =
+            "<invalid>1</tags>"
+
+        val formattedResponse =
+            formatter.format(xml)
+
+        assertThat(
+            formattedResponse,
+            `is`(xml)
+        )
+    }
 }
