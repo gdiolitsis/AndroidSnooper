46cfde6b5a50700fc57b95a25f5ac43c8693155c
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:41:55 +0300

    Update HttpCallRecord.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpCallRecord.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpCallRecord.kt
@@ -3,49 +3,113 @@ package com.prateekj.snooper.networksnooper.model
 import java.util.Date
 
 class HttpCallRecord {
-  var id: Long = 0
-  var url: String? = null
-  var payload: String? = null
-  var method: String? = null
-  var responseBody: String? = null
-  var statusText: String? = null
-  var statusCode: Int = 0
-  var date: Date? = null
-  var error: String? = null
-  var requestHeaders: List<HttpHeader>? = null
-  var responseHeaders: List<HttpHeader>? = null
 
-  fun getResponseHeader(name: String): HttpHeader? {
-    return filterFromCollection(name, responseHeaders)
-  }
+    var id: Long = 0
 
-  fun getRequestHeader(name: String): HttpHeader? {
-    return filterFromCollection(name, requestHeaders)
-  }
+    var url: String? = null
 
-  private fun filterFromCollection(name: String, collection: List<HttpHeader>?): HttpHeader? {
-    return collection?.firstOrNull { header -> header.name.equals(name, ignoreCase = true) }
-  }
+    var payload: String? = null
 
-  fun hasError(): Boolean {
-    return error != null
-  }
+    var method: String? = null
 
-  companion object {
+    var responseBody: String? = null
 
-    fun from(httpCall: HttpCall): HttpCallRecord {
-      val httpCallRecord = HttpCallRecord()
-      httpCallRecord.url = httpCall.url
-      httpCallRecord.payload = httpCall.payload
-      httpCallRecord.method = httpCall.method
-      httpCallRecord.responseBody = httpCall.responseBody
-      httpCallRecord.statusText = httpCall.statusText
-      httpCallRecord.statusCode = httpCall.statusCode
-      httpCallRecord.date = httpCall.date
-      httpCallRecord.error = httpCall.error
-      httpCallRecord.requestHeaders = HttpHeader.from(httpCall.requestHeaders)
-      httpCallRecord.responseHeaders = HttpHeader.from(httpCall.responseHeaders)
-      return httpCallRecord
+    var statusText: String? = null
+
+    var statusCode: Int = 0
+
+    var date: Date? = null
+
+    var error: String? = null
+
+    var requestHeaders:
+            List<HttpHeader>? = null
+
+    var responseHeaders:
+            List<HttpHeader>? = null
+
+    fun getResponseHeader(
+        name: String
+    ): HttpHeader? {
+
+        return filterFromCollection(
+            name,
+            responseHeaders
+        )
     }
-  }
+
+    fun getRequestHeader(
+        name: String
+    ): HttpHeader? {
+
+        return filterFromCollection(
+            name,
+            requestHeaders
+        )
+    }
+
+    private fun filterFromCollection(
+        name: String,
+        collection: List<HttpHeader>?
+    ): HttpHeader? {
+
+        return collection?.firstOrNull {
+
+            it.name.equals(
+                name,
+                ignoreCase = true
+            )
+        }
+    }
+
+    fun hasError():
+            Boolean {
+
+        return error != null
+    }
+
+    companion object {
+
+        fun from(
+            httpCall: HttpCall
+        ): HttpCallRecord {
+
+            return HttpCallRecord().apply {
+
+                url =
+                    httpCall.url
+
+                payload =
+                    httpCall.payload
+
+                method =
+                    httpCall.method
+
+                responseBody =
+                    httpCall.responseBody
+
+                statusText =
+                    httpCall.statusText
+
+                statusCode =
+                    httpCall.statusCode
+
+                date =
+                    httpCall.date
+
+                error =
+                    httpCall.error
+
+                requestHeaders =
+                    HttpHeader.from(
+                        httpCall.requestHeaders
+                    )
+
+                responseHeaders =
+                    HttpHeader.from(
+                        httpCall.responseHeaders
+                    )
+            }
+        }
+    }
 }
