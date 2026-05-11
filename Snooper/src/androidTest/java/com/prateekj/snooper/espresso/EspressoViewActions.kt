01a48766a375d11abd961a14d1a640d2ceef3262
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 00:50:32 +0300

    Update EspressoViewActions.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/espresso/EspressoViewActions.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/espresso/EspressoViewActions.kt
@@ -1,17 +1,32 @@
 package com.prateekj.snooper.espresso
 
-import androidx.test.espresso.ViewAction
 import android.view.View
-
+import androidx.test.espresso.ViewAction
 import org.hamcrest.Matcher
 
 object EspressoViewActions {
-  const val ONE_SECOND = 1000
-  fun waitFor(viewMatcher: Matcher<View>): ViewAction {
-    return WaitForViewAction(viewMatcher, ONE_SECOND * 5)
-  }
 
-  fun waitFor(viewMatcher: Matcher<View>, timeout: Int): ViewAction {
-    return WaitForViewAction(viewMatcher, timeout)
-  }
+    const val ONE_SECOND =
+        1000
+
+    fun waitFor(
+        viewMatcher: Matcher<View>
+    ): ViewAction {
+
+        return WaitForViewAction(
+            viewMatcher,
+            ONE_SECOND * 5
+        )
+    }
+
+    fun waitFor(
+        viewMatcher: Matcher<View>,
+        timeout: Int
+    ): ViewAction {
+
+        return WaitForViewAction(
+            viewMatcher,
+            timeout
+        )
+    }
 }
