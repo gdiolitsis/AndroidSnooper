7de7b1a10763df75dd8b221993dbd30313e8be98
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 02:00:13 +0300

    Update HttpCallRecord.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpCallRecord.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpCallRecord.kt
@@ -2,31 +2,32 @@ package com.prateekj.snooper.networksnooper.model
 
 import java.util.Date
 
-class HttpCallRecord {
+data class HttpCallRecord(
 
-    var id: Long = 0
+    var id: Long = 0,
 
-    var url: String? = null
+    var url: String? = null,
 
-    var payload: String? = null
+    var payload: String? = null,
 
-    var method: String? = null
+    var method: String? = null,
 
-    var responseBody: String? = null
+    var responseBody: String? = null,
 
-    var statusText: String? = null
+    var statusText: String? = null,
 
-    var statusCode: Int = 0
+    var statusCode: Int = 0,
 
-    var date: Date? = null
+    var date: Date? = null,
 
-    var error: String? = null
+    var error: String? = null,
 
     var requestHeaders:
-            List<HttpHeader>? = null
+            List<HttpHeader>? = null,
 
     var responseHeaders:
             List<HttpHeader>? = null
+) {
 
     fun getResponseHeader(
         name: String
@@ -53,19 +54,20 @@ class HttpCallRecord {
         collection: List<HttpHeader>?
     ): HttpHeader? {
 
-        return collection?.firstOrNull {
+        return collection
+            ?.firstOrNull { header ->
 
-            it.name.equals(
-                name,
-                ignoreCase = true
-            )
-        }
+                header.name.equals(
+                    name,
+                    ignoreCase = true
+                )
+            }
     }
 
     fun hasError():
             Boolean {
 
-        return error != null
+        return !error.isNullOrBlank()
     }
 
     companion object {
@@ -74,42 +76,22 @@ class HttpCallRecord {
             httpCall: HttpCall
         ): HttpCallRecord {
 
-            return HttpCallRecord().apply {
-
-                url =
-                    httpCall.url
-
-                payload =
-                    httpCall.payload
-
-                method =
-                    httpCall.method
-
-                responseBody =
-                    httpCall.responseBody
-
-                statusText =
-                    httpCall.statusText
-
-                statusCode =
-                    httpCall.statusCode
-
-                date =
-                    httpCall.date
-
-                error =
-                    httpCall.error
-
-                requestHeaders =
-                    HttpHeader.from(
-                        httpCall.requestHeaders
-                    )
-
-                responseHeaders =
-                    HttpHeader.from(
-                        httpCall.responseHeaders
-                    )
-            }
+            return HttpCallRecord(
+                url = httpCall.url,
+                payload = httpCall.payload,
+                method = httpCall.method,
+                responseBody = httpCall.responseBody,
+                statusText = httpCall.statusText,
+                statusCode = httpCall.statusCode,
+                date = httpCall.date,
+                error = httpCall.error,
+                requestHeaders = HttpHeader.from(
+                    httpCall.requestHeaders
+                ),
+                responseHeaders = HttpHeader.from(
+                    httpCall.responseHeaders
+                )
+            )
         }
     }
 }
