86c7bd714ccc65eb948b0b05733902626274cb24
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:48:58 +0300

    Update CurrentActivityManager.kt

--- Snooper/src/main/java/com/prateekj/snooper/infra/CurrentActivityManager.kt
+++ Snooper/src/main/java/com/prateekj/snooper/infra/CurrentActivityManager.kt
@@ -1,66 +1,124 @@
 package com.prateekj.snooper.infra
 
-
 import android.app.Activity
 import android.app.Application
 import android.app.Application.ActivityLifecycleCallbacks
 import android.os.Bundle
+import java.util.concurrent.CopyOnWriteArrayList
 
-import java.util.ArrayList
+class CurrentActivityManager private constructor(
+    application: Application
+) : ActivityLifecycleCallbacks {
 
-class CurrentActivityManager private constructor(application: Application) :
-  ActivityLifecycleCallbacks {
+    private val listeners =
+        CopyOnWriteArrayList<Listener>()
 
-  private val listeners = ArrayList<Listener>()
+    interface Listener {
 
-  interface Listener {
-    fun currentActivity(activity: Activity?)
-  }
-
-  init {
-    application.registerActivityLifecycleCallbacks(this)
-  }
-
-  fun registerListener(listener: Listener) {
-    listeners.add(listener)
-  }
-
-  fun unregisterListener(listener: Listener) {
-    listeners.remove(listener)
-  }
-
-
-  override fun onActivityResumed(activity: Activity?) {
-    notifyListeners(activity)
-  }
-
-
-  override fun onActivityPaused(activity: Activity?) {
-    notifyListeners(activity)
-  }
-
-  private fun notifyListeners(activity: Activity?) {
-    for (listener in listeners) {
-      listener.currentActivity(activity)
+        fun currentActivity(
+            activity: Activity?
+        )
     }
-  }
 
-  override fun onActivityStopped(activity: Activity?) {}
+    init {
 
-  override fun onActivityCreated(activity: Activity?, savedInstanceState: Bundle?) {}
-
-  override fun onActivityStarted(activity: Activity?) {}
-
-  override fun onActivitySaveInstanceState(activity: Activity?, outState: Bundle?) {}
-
-  override fun onActivityDestroyed(activity: Activity?) {}
-
-  companion object {
-
-    private var INSTANCE: CurrentActivityManager? = null
-
-    fun getInstance(application: Application): CurrentActivityManager {
-      return INSTANCE ?: CurrentActivityManager(application).also { INSTANCE = it }
+        application.registerActivityLifecycleCallbacks(
+            this
+        )
     }
-  }
+
+    fun registerListener(
+        listener: Listener
+    ) {
+
+        if (!listeners.contains(listener)) {
+
+            listeners.add(listener)
+        }
+    }
+
+    fun unregisterListener(
+        listener: Listener
+    ) {
+
+        listeners.remove(listener)
+    }
+
+    override fun onActivityResumed(
+        activity: Activity
+    ) {
+
+        notifyListeners(activity)
+    }
+
+    override fun onActivityPaused(
+        activity: Activity
+    ) {
+
+        notifyListeners(null)
+    }
+
+    private fun notifyListeners(
+        activity: Activity?
+    ) {
+
+        for (listener in listeners) {
+
+            try {
+
+                listener.currentActivity(activity)
+
+            } catch (_: Exception) {
+            }
+        }
+    }
+
+    override fun onActivityStopped(
+        activity: Activity
+    ) {
+    }
+
+    override fun onActivityCreated(
+        activity: Activity,
+        savedInstanceState: Bundle?
+    ) {
+    }
+
+    override fun onActivityStarted(
+        activity: Activity
+    ) {
+    }
+
+    override fun onActivitySaveInstanceState(
+        activity: Activity,
+        outState: Bundle
+    ) {
+    }
+
+    override fun onActivityDestroyed(
+        activity: Activity
+    ) {
+    }
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE:
+                CurrentActivityManager? = null
+
+        fun getInstance(
+            application: Application
+        ): CurrentActivityManager {
+
+            return INSTANCE ?: synchronized(this) {
+
+                INSTANCE ?: CurrentActivityManager(
+                    application
+                ).also {
+
+                    INSTANCE = it
+                }
+            }
+        }
+    }
 }
