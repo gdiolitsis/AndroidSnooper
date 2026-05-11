2ce4bba31ecc68518b9f68dfd5f760e4d44ef381
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:38:39 +0300

    Update HttpHeadersFragment.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/fragment/HttpHeadersFragment.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/fragment/HttpHeadersFragment.kt
@@ -1,6 +1,5 @@
 package com.prateekj.snooper.networksnooper.fragment
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,32 +13,76 @@ import com.prateekj.snooper.networksnooper.viewmodel.HttpCallViewModel
 import kotlinx.android.synthetic.main.fragment_headers.view.*
 import kotlinx.android.synthetic.main.http_call_general_detail.view.*
 
-class HttpHeadersFragment : Fragment() {
+class HttpHeadersFragment :
+    Fragment() {
 
-  override fun onCreateView(
-    inflater: LayoutInflater,
-    container: ViewGroup?,
-    savedInstanceState: Bundle?
-  ): View? {
-    super.onCreateView(inflater, container, savedInstanceState)
-    val view = inflater.inflate(R.layout.fragment_headers, container, false)
-    val snooperRepo = SnooperRepo(activity!!)
-    val httpCallId = arguments!!.getLong(HTTP_CALL_ID)
-    val httpCallRecord = snooperRepo.findById(httpCallId)
-    val httpCallViewModel = HttpCallViewModel(httpCallRecord)
-    view.url.text = httpCallViewModel.url
-    view.method.text = httpCallViewModel.method
-    view.status_code.text = httpCallViewModel.statusCode
-    view.status_text.text = httpCallViewModel.statusText
-    view.time_stamp.text = httpCallViewModel.timeStamp
-    view.response_info_container.visibility = httpCallViewModel.responseInfoVisibility
-    view.error_text.visibility = httpCallViewModel.failedTextVisibility
-    view.response_header_list.adapter =
-      HttpHeaderAdapter.newInstance(httpCallViewModel.responseHeaders)
-    view.request_header_list.adapter =
-      HttpHeaderAdapter.newInstance(httpCallViewModel.requestHeaders)
-    view.response_header_container.visibility = httpCallViewModel.responseHeaderVisibility
-    view.request_header_container.visibility = httpCallViewModel.requestHeaderVisibility
-    return view
-  }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        val view =
+            inflater.inflate(
+                R.layout.fragment_headers,
+                container,
+                false
+            )
+
+        val repo =
+            SnooperRepo(
+                requireActivity()
+            )
+
+        val httpCallId =
+            requireArguments()
+                .getLong(HTTP_CALL_ID)
+
+        val httpCallRecord =
+            repo.findById(httpCallId)
+
+        val viewModel =
+            HttpCallViewModel(
+                httpCallRecord
+            )
+
+        view.url.text =
+            viewModel.url
+
+        view.method.text =
+            viewModel.method
+
+        view.status_code.text =
+            viewModel.statusCode
+
+        view.status_text.text =
+            viewModel.statusText
+
+        view.time_stamp.text =
+            viewModel.timeStamp
+
+        view.response_info_container.visibility =
+            viewModel.responseInfoVisibility
+
+        view.error_text.visibility =
+            viewModel.failedTextVisibility
+
+        view.response_header_list.adapter =
+            HttpHeaderAdapter.newInstance(
+                viewModel.responseHeaders
+            )
+
+        view.request_header_list.adapter =
+            HttpHeaderAdapter.newInstance(
+                viewModel.requestHeaders
+            )
+
+        view.response_header_container.visibility =
+            viewModel.responseHeaderVisibility
+
+        view.request_header_container.visibility =
+            viewModel.requestHeaderVisibility
+
+        return view
+    }
 }
