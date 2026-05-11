ca8aaf3035c22ddaa062e15e737561fef1f8448d
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:08:27 +0300

    Update EspressoViewMatchers.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/utils/EspressoViewMatchers.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/utils/EspressoViewMatchers.kt
@@ -1,85 +1,161 @@
 package com.prateekj.snooper.utils
 
-import android.text.SpannableString
+import android.text.Spannable
 import android.text.style.BackgroundColorSpan
 import android.view.View
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
 import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import org.hamcrest.CustomTypeSafeMatcher
 import org.hamcrest.Matcher
 
 object EspressoViewMatchers {
 
-  fun withRecyclerView(recyclerViewId: Int, position: Int): Matcher<View> {
+    fun withRecyclerView(
+        recyclerViewId: Int,
+        position: Int
+    ): Matcher<View> {
 
-    return object : CustomTypeSafeMatcher<View>(
-      "recycler view with id: $recyclerViewId at position: $position"
-    ) {
-      override fun matchesSafely(item: View): Boolean {
-        val view = item.rootView.findViewById<View>(recyclerViewId)
+        return object : CustomTypeSafeMatcher<View>(
+            "RecyclerView id=$recyclerViewId position=$position"
+        ) {
 
-        if (view == null || view !is RecyclerView)
-          return false
-        val childView = view.findViewHolderForAdapterPosition(position)!!.itemView
-        return childView === item
-      }
-    }
-  }
+            override fun matchesSafely(
+                item: View
+            ): Boolean {
 
-  fun withTableLayout(tableLayoutId: Int, row: Int, column: Int): Matcher<View> {
-    return object : CustomTypeSafeMatcher<View>(
-      "Table layout with id: $tableLayoutId at row: $row and column: $column"
-    ) {
-      override fun matchesSafely(item: View): Boolean {
-        val view = item.rootView.findViewById<View>(tableLayoutId)
+                val recyclerView =
+                    item.rootView.findViewById<RecyclerView>(
+                        recyclerViewId
+                    ) ?: return false
 
-        if (view == null || view !is TableLayout)
-          return false
-        val tableRow = view.getChildAt(row) as TableRow
-        val childView = tableRow.getChildAt(column)
-        return childView === item
-      }
-    }
-  }
+                val viewHolder =
+                    recyclerView.findViewHolderForAdapterPosition(
+                        position
+                    ) ?: return false
 
-  fun withListSize(size: Int): Matcher<View> {
-    return object : CustomTypeSafeMatcher<View>(
-      "recycler view with id: $size "
-    ) {
-      override fun matchesSafely(view: View?): Boolean {
-
-        if (view == null || view !is RecyclerView)
-          return false
-        val recyclerView = view as RecyclerView?
-        return recyclerView!!.adapter!!.itemCount == size
-      }
-    }
-  }
-
-  fun hasBackgroundSpanOn(text: String, @ColorRes colorResource: Int): Matcher<View> {
-    return object : CustomTypeSafeMatcher<View>("") {
-      override fun matchesSafely(view: View?): Boolean {
-        if (view == null || view !is TextView)
-          return false
-        val spannableString = view.text as SpannableString
-        val spans = spannableString.getSpans(
-          0,
-          spannableString.length,
-          BackgroundColorSpan::class.java
-        )
-        for (span in spans) {
-          val start = spannableString.getSpanStart(span)
-          val end = spannableString.getSpanEnd(span)
-          val highlightedString = spannableString.subSequence(start, end)
-          if (text == highlightedString.toString()) {
-            return span.backgroundColor == view.context.resources.getColor(colorResource)
-          }
+                return viewHolder.itemView === item
+            }
         }
-        return false
-      }
     }
-  }
+
+    fun withTableLayout(
+        tableLayoutId: Int,
+        row: Int,
+        column: Int
+    ): Matcher<View> {
+
+        return object : CustomTypeSafeMatcher<View>(
+            "TableLayout id=$tableLayoutId row=$row column=$column"
+        ) {
+
+            override fun matchesSafely(
+                item: View
+            ): Boolean {
+
+                val tableLayout =
+                    item.rootView.findViewById<TableLayout>(
+                        tableLayoutId
+                    ) ?: return false
+
+                val tableRow =
+                    tableLayout.getChildAt(row)
+                            as? TableRow
+                        ?: return false
+
+                val childView =
+                    tableRow.getChildAt(column)
+                        ?: return false
+
+                return childView === item
+            }
+        }
+    }
+
+    fun withListSize(
+        size: Int
+    ): Matcher<View> {
+
+        return object : CustomTypeSafeMatcher<View>(
+            "RecyclerView with size=$size"
+        ) {
+
+            override fun matchesSafely(
+                view: View?
+            ): Boolean {
+
+                val recyclerView =
+                    view as? RecyclerView
+                        ?: return false
+
+                return recyclerView
+                    .adapter
+                    ?.itemCount == size
+            }
+        }
+    }
+
+    fun hasBackgroundSpanOn(
+        text: String,
+        @ColorRes colorResource: Int
+    ): Matcher<View> {
+
+        return object : CustomTypeSafeMatcher<View>(
+            "Text contains highlighted span"
+        ) {
+
+            override fun matchesSafely(
+                view: View?
+            ): Boolean {
+
+                val textView =
+                    view as? TextView
+                        ?: return false
+
+                val spannable =
+                    textView.text as? Spannable
+                        ?: return false
+
+                val expectedColor =
+                    ContextCompat.getColor(
+                        textView.context,
+                        colorResource
+                    )
+
+                val spans =
+                    spannable.getSpans(
+                        0,
+                        spannable.length,
+                        BackgroundColorSpan::class.java
+                    )
+
+                for (span in spans) {
+
+                    val start =
+                        spannable.getSpanStart(span)
+
+                    val end =
+                        spannable.getSpanEnd(span)
+
+                    val highlightedText =
+                        spannable.subSequence(
+                            start,
+                            end
+                        ).toString()
+
+                    if (highlightedText == text &&
+                        span.backgroundColor == expectedColor
+                    ) {
+
+                        return true
+                    }
+                }
+
+                return false
+            }
+        }
+    }
 }
