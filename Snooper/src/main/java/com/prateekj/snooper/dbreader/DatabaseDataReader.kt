5cd672142f23eb24db882a5e97674a7b51b0e7ee
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:27:04 +0300

    Update DatabaseDataReader.kt

--- Snooper/src/main/java/com/prateekj/snooper/dbreader/DatabaseDataReader.kt
+++ Snooper/src/main/java/com/prateekj/snooper/dbreader/DatabaseDataReader.kt
@@ -4,53 +4,104 @@ import android.database.sqlite.SQLiteDatabase
 import com.prateekj.snooper.dbreader.model.Database
 import com.prateekj.snooper.dbreader.model.Row
 import com.prateekj.snooper.dbreader.model.Table
-import java.util.ArrayList
-import java.util.Arrays
 
 class DatabaseDataReader {
 
-  fun getData(database: SQLiteDatabase): Database {
-    val databaseData = Database().also {
-      it.path = database.path
-      it.version = database.version
-    }
-    val tableNameCursor =
-      database.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null)
-    val tables = ArrayList<String>()
-    tableNameCursor.use { cursor ->
-      if (cursor != null && cursor.moveToFirst()) {
-        while (!cursor.isAfterLast) {
-          tables.add(cursor.getString(cursor.getColumnIndexOrThrow("name")))
-          cursor.moveToNext()
-        }
-        databaseData.tables = tables
-      }
-    }
-    return databaseData
-  }
+    fun getData(
+        database: SQLiteDatabase
+    ): Database {
 
-  fun getTableData(database: SQLiteDatabase, tableName: String): Table {
-    val rows = ArrayList<Row>()
-    val table = Table()
-    val tableDataCursor = database.rawQuery("SELECT * FROM $tableName", null)
+        val databaseData =
+            Database().apply {
 
-    tableDataCursor.use { cursor ->
-      table.columns = listOf(*cursor!!.columnNames)
-      if (cursor.moveToFirst()) {
-        val columnCount = cursor.columnCount
-        while (!cursor.isAfterLast) {
-          val data = ArrayList<String>()
-          for (i in 0 until columnCount) {
-            data.add(cursor.getString(i))
-          }
-          val row = Row(data)
-          rows.add(row)
-          cursor.moveToNext()
+                path = database.path
+                version = database.version
+            }
+
+        val tableNames =
+            mutableListOf<String>()
+
+        database.rawQuery(
+            "SELECT name FROM sqlite_master WHERE type='table'",
+            null
+        ).use { cursor ->
+
+            if (cursor.moveToFirst()) {
+
+                val nameColumnIndex =
+                    cursor.getColumnIndexOrThrow(
+                        "name"
+                    )
+
+                do {
+
+                    tableNames.add(
+                        cursor.getString(
+                            nameColumnIndex
+                        )
+                    )
+
+                } while (cursor.moveToNext())
+            }
         }
-      }
-      table.name = tableName
-      table.rows = rows
+
+        databaseData.tables =
+            tableNames
+
+        return databaseData
     }
-    return table
-  }
+
+    fun getTableData(
+        database: SQLiteDatabase,
+        tableName: String
+    ): Table {
+
+        val table =
+            Table()
+
+        val rows =
+            mutableListOf<Row>()
+
+        database.rawQuery(
+            "SELECT * FROM $tableName",
+            null
+        ).use { cursor ->
+
+            table.columns =
+                cursor.columnNames.toList()
+
+            if (cursor.moveToFirst()) {
+
+                val columnCount =
+                    cursor.columnCount
+
+                do {
+
+                    val data =
+                        mutableListOf<String>()
+
+                    for (i in 0 until columnCount) {
+
+                        data.add(
+                            cursor.getString(i)
+                                ?: ""
+                        )
+                    }
+
+                    rows.add(
+                        Row(data)
+                    )
+
+                } while (cursor.moveToNext())
+            }
+        }
+
+        table.name =
+            tableName
+
+        table.rows =
+            rows
+
+        return table
+    }
 }
