cf7f404233d1c418f0f2c3271f25aba4708fa4d6
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:58:54 +0300

    Update DatabaseReaderTest.kt

--- Snooper/src/test/java/com/prateekj/snooper/dbreader/DatabaseReaderTest.kt
+++ Snooper/src/test/java/com/prateekj/snooper/dbreader/DatabaseReaderTest.kt
@@ -7,6 +7,7 @@ import com.prateekj.snooper.infra.BackgroundTask
 import com.prateekj.snooper.infra.BackgroundTaskExecutor
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import org.hamcrest.CustomTypeSafeMatcher
 import org.hamcrest.Matcher
 import org.junit.Assert.assertEquals
@@ -16,92 +17,241 @@ import org.junit.Test
 import java.io.File
 
 class DatabaseReaderTest {
-  private lateinit var context: Context
-  private lateinit var applicationContext: Context
-  private lateinit var backgroundTaskExecutor: BackgroundTaskExecutor
-  private lateinit var dbReaderCallback: DbReaderCallback
-  private lateinit var databaseDataReader: DatabaseDataReader
-  private lateinit var databaseReader: DatabaseReader
 
-  @Before
-  @Throws(Exception::class)
-  fun setUp() {
-    context = mockk(relaxed = true)
-    applicationContext = mockk(relaxed = true)
-    backgroundTaskExecutor = mockk(relaxed = true)
-    dbReaderCallback = mockk(relaxed = true)
-    databaseDataReader = mockk(relaxed = true)
-    every { context.applicationContext } returns applicationContext
-    databaseReader = DatabaseReader(context, backgroundTaskExecutor, databaseDataReader)
-  }
+    private lateinit var context: Context
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldReturnEmptyListIfNoDbFilesPresent() {
-    val databases = arrayOfNulls<String>(0)
-    every { applicationContext.databaseList() } returns databases
-    resolveBackgroundTask()
+    private lateinit var applicationContext:
+            Context
 
-    databaseReader.fetchApplicationDatabases(dbReaderCallback)
+    private lateinit var backgroundTaskExecutor:
+            BackgroundTaskExecutor
 
-    io.mockk.verify { dbReaderCallback.onApplicationDbFetchCompleted(match { it.size == 0 }) }
-  }
+    private lateinit var dbReaderCallback:
+            DbReaderCallback
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldReturnEmptyListIfDatabaseListIsNull() {
-    every { applicationContext.databaseList() } returns null
-    resolveBackgroundTask()
+    private lateinit var databaseDataReader:
+            DatabaseDataReader
 
-    databaseReader.fetchApplicationDatabases(dbReaderCallback)
+    private lateinit var databaseReader:
+            DatabaseReader
 
-    io.mockk.verify { dbReaderCallback.onApplicationDbFetchCompleted(match { it.size == 0 }) }
-  }
+    @Before
+    fun setUp() {
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldFilterOutListOfDbFilesPresent() {
-    val databases = arrayOf("app.db", "user.db", "app.journal", "/app.tmp", "snooper.db")
-    every { applicationContext.databaseList() } returns databases
-    val file1 = getMockFile("/location1/app.db", "app.db")
-    val file2 = getMockFile("/location2/user.db", "user.db")
+        context =
+            mockk(relaxed = true)
 
-    every { context.getDatabasePath("app.db") } returns file1
-    every { context.getDatabasePath("user.db") } returns file2
-    resolveBackgroundTask()
+        applicationContext =
+            mockk(relaxed = true)
 
-    databaseReader.fetchApplicationDatabases(dbReaderCallback)
+        backgroundTaskExecutor =
+            mockk(relaxed = true)
 
+        dbReaderCallback =
+            mockk(relaxed = true)
 
-    io.mockk.verify {
-      dbReaderCallback.onApplicationDbFetchCompleted(match {
-        assertEquals(it.size, 2)
-        assertThat(it[0], isSameAsDatabaseWithParameters("/location1/app.db", "app.db"))
-        assertThat(it[1], isSameAsDatabaseWithParameters("/location2/user.db", "user.db"))
-        true
-      })
+        databaseDataReader =
+            mockk(relaxed = true)
+
+        every {
+            context.applicationContext
+        } returns applicationContext
+
+        databaseReader =
+            DatabaseReader(
+                context,
+                backgroundTaskExecutor,
+                databaseDataReader
+            )
     }
-  }
 
-  private fun isSameAsDatabaseWithParameters(path: String, name: String): Matcher<Database> {
-    return object : CustomTypeSafeMatcher<Database>("a Database with name $name") {
-      public override fun matchesSafely(item: Database): Boolean {
-        return name == item.name && path == item.path
-      }
+    @Test
+    fun shouldReturnEmptyListIfNoDbFilesPresent() {
+
+        val databases =
+            arrayOfNulls<String>(0)
+
+        every {
+            applicationContext.databaseList()
+        } returns databases
+
+        resolveBackgroundTask()
+
+        databaseReader
+            .fetchApplicationDatabases(
+                dbReaderCallback
+            )
+
+        verify {
+
+            dbReaderCallback
+                .onApplicationDbFetchCompleted(
+                    match {
+                        it.isEmpty()
+                    }
+                )
+        }
     }
-  }
 
-  private fun getMockFile(path: String, dbName: String): File {
-    val file = mockk<File>(relaxed = true)
-    every { file.absolutePath } returns path
-    every { file.name } returns dbName
-    return file
-  }
+    @Test
+    fun shouldReturnEmptyListIfDatabaseListIsNull() {
 
-  private fun resolveBackgroundTask() {
-    every { backgroundTaskExecutor.execute(any<BackgroundTask<String>>()) } answers {
-      val backgroundTask = this.firstArg<BackgroundTask<String>>()
-      backgroundTask.onResult(backgroundTask.onExecute())
+        every {
+            applicationContext.databaseList()
+        } returns null
+
+        resolveBackgroundTask()
+
+        databaseReader
+            .fetchApplicationDatabases(
+                dbReaderCallback
+            )
+
+        verify {
+
+            dbReaderCallback
+                .onApplicationDbFetchCompleted(
+                    match {
+                        it.isEmpty()
+                    }
+                )
+        }
     }
-  }
+
+    @Test
+    fun shouldFilterOutListOfDbFilesPresent() {
+
+        val databases =
+            arrayOf(
+                "app.db",
+                "user.db",
+                "app.journal",
+                "/app.tmp",
+                "snooper.db"
+            )
+
+        every {
+            applicationContext.databaseList()
+        } returns databases
+
+        val file1 =
+            getMockFile(
+                "/location1/app.db",
+                "app.db"
+            )
+
+        val file2 =
+            getMockFile(
+                "/location2/user.db",
+                "user.db"
+            )
+
+        every {
+            context.getDatabasePath("app.db")
+        } returns file1
+
+        every {
+            context.getDatabasePath("user.db")
+        } returns file2
+
+        resolveBackgroundTask()
+
+        databaseReader
+            .fetchApplicationDatabases(
+                dbReaderCallback
+            )
+
+        verify {
+
+            dbReaderCallback
+                .onApplicationDbFetchCompleted(
+                    match {
+
+                        assertEquals(
+                            2,
+                            it.size
+                        )
+
+                        assertThat(
+                            it[0],
+                            isSameAsDatabaseWithParameters(
+                                "/location1/app.db",
+                                "app.db"
+                            )
+                        )
+
+                        assertThat(
+                            it[1],
+                            isSameAsDatabaseWithParameters(
+                                "/location2/user.db",
+                                "user.db"
+                            )
+                        )
+
+                        true
+                    }
+                )
+        }
+    }
+
+    private fun isSameAsDatabaseWithParameters(
+        path: String,
+        name: String
+    ): Matcher<Database> {
+
+        return object :
+            CustomTypeSafeMatcher<Database>(
+                "a Database with name $name"
+            ) {
+
+            override fun matchesSafely(
+                item: Database
+            ): Boolean {
+
+                return (
+                    item.name == name
+                    &&
+                    item.path == path
+                )
+            }
+        }
+    }
+
+    private fun getMockFile(
+        path: String,
+        dbName: String
+    ): File {
+
+        val file =
+            mockk<File>(relaxed = true)
+
+        every {
+            file.absolutePath
+        } returns path
+
+        every {
+            file.name
+        } returns dbName
+
+        return file
+    }
+
+    private fun resolveBackgroundTask() {
+
+        every {
+            backgroundTaskExecutor.execute(
+                any<BackgroundTask<List<Database>>>()
+            )
+        } answers {
+
+            val backgroundTask =
+                firstArg<
+                    BackgroundTask<List<Database>>
+                    >()
+
+            backgroundTask.onResult(
+                backgroundTask.onExecute()
+            )
+        }
+    }
 }
