54625bbb688732d31da11c3082c1576b2737e638
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 14:07:24 +0300

    Update push-artifact.sh

--- travis/push-artifact.sh
+++ travis/push-artifact.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+
+set -e
+
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-    echo "Pull request: Skipping test artifact deployment:"
-else
-    zip -r reports.zip Snooper/build/spoon
-    curl -X POST https://content.dropboxapi.com/2/files/upload \
-        --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
-        --header "Dropbox-API-Arg: {\"path\": \"/builds/reports_${TRAVIS_BUILD_NUMBER}.zip\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
-        --header "Content-Type: application/octet-stream" \
-        --data-binary @reports.zip
+
+    echo "Pull request detected. Skipping test artifact deployment."
+
+    exit 0
 fi
+
+if [ ! -d "Snooper/build/spoon" ]; then
+
+    echo "Directory Snooper/build/spoon not found."
+
+    exit 1
+fi
+
+zip -r reports.zip Snooper/build/spoon
+
+curl -X POST \
+    https://content.dropboxapi.com/2/files/upload \
+    --header "Authorization: Bearer ${DROPBOX_ACCESS_TOKEN}" \
+    --header "Dropbox-API-Arg: {\"path\": \"/builds/reports_${TRAVIS_BUILD_NUMBER}.zip\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}" \
+    --header "Content-Type: application/octet-stream" \
+    --data-binary @reports.zip
