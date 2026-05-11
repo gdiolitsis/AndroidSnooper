4ab55ba66e33ea3421ca8f687d3ffd4194959e97
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:48:33 +0300

    Update HttpCallViewModel.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/viewmodel/HttpCallViewModel.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/viewmodel/HttpCallViewModel.kt
@@ -9,52 +9,131 @@ import com.prateekj.snooper.networksnooper.model.HttpHeader
 import java.text.SimpleDateFormat
 import java.util.Locale.US
 
-class HttpCallViewModel(private val httpCall: HttpCallRecord) {
+class HttpCallViewModel(
+    private val httpCall:
+            HttpCallRecord
+) {
 
-  val url: String? = httpCall.url
+    val url: String? =
+        httpCall.url
 
-  val method: String? = httpCall.method
+    val method: String? =
+        httpCall.method
 
-  val statusCode: String = httpCall.statusCode.toString()
+    val statusCode: String =
+        httpCall.statusCode.toString()
 
-  val statusText: String? = httpCall.statusText
+    val statusText: String? =
+        httpCall.statusText
 
-  val requestHeaders: List<HttpHeader> = httpCall.requestHeaders ?: listOf()
+    val requestHeaders:
+            List<HttpHeader> =
+        httpCall.requestHeaders
+            ?: emptyList()
 
-  val responseHeaders: List<HttpHeader> = httpCall.responseHeaders ?: listOf()
+    val responseHeaders:
+            List<HttpHeader> =
+        httpCall.responseHeaders
+            ?: emptyList()
 
-  val timeStamp: String
-    get() {
-      val df = SimpleDateFormat(TIMESTAMP_FORMAT, US)
-      return df.format(httpCall.date)
+    val timeStamp: String
+        get() {
+
+            val dateFormat =
+                SimpleDateFormat(
+                    TIMESTAMP_FORMAT,
+                    US
+                )
+
+            return dateFormat.format(
+                httpCall.date
+            )
+        }
+
+    val responseInfoVisibility: Int =
+        if (httpCall.hasError()) {
+            GONE
+        } else {
+            VISIBLE
+        }
+
+    val failedTextVisibility: Int =
+        if (httpCall.hasError()) {
+            VISIBLE
+        } else {
+            GONE
+        }
+
+    val responseHeaderVisibility: Int =
+        if (
+            hasHeaders(
+                httpCall.responseHeaders
+            )
+        ) {
+            VISIBLE
+        } else {
+            GONE
+        }
+
+    val requestHeaderVisibility: Int =
+        if (
+            hasHeaders(
+                httpCall.requestHeaders
+            )
+        ) {
+            VISIBLE
+        } else {
+            GONE
+        }
+
+    @ColorRes
+    fun getStatusColor():
+            Int {
+
+        val statusCode =
+            httpCall.statusCode
+
+        return when {
+
+            statusCode in
+                    RANGE_START_HTTP_OK..
+                    RANGE_END_HTTP_OK -> {
+
+                R.color.snooper_green
+            }
+
+            statusCode <=
+                    RANGE_END_HTTP_REDIRECTION -> {
+
+                R.color.snooper_yellow
+            }
+
+            else -> {
+
+                R.color.snooper_red
+            }
+        }
     }
 
-  val responseInfoVisibility: Int = if (httpCall.hasError()) GONE else VISIBLE
+    private fun hasHeaders(
+        headers: List<HttpHeader>?
+    ): Boolean {
 
-  val failedTextVisibility: Int = if (httpCall.hasError()) VISIBLE else GONE
-
-  val responseHeaderVisibility: Int = if (hasHeaders(httpCall.responseHeaders)) VISIBLE else GONE
-
-  val requestHeaderVisibility: Int = if (hasHeaders(httpCall.requestHeaders)) VISIBLE else GONE
-
-  @ColorRes
-  fun getStatusColor(): Int {
-    val statusCode = httpCall.statusCode
-    return when {
-      statusCode in RANGE_START_HTTP_OK..RANGE_END_HTTP_OK -> R.color.snooper_green
-      statusCode <= RANGE_END_HTTP_REDIRECTION -> R.color.snooper_yellow
-      else -> R.color.snooper_red
+        return !headers.isNullOrEmpty()
     }
-  }
 
-  private fun hasHeaders(headers: List<HttpHeader>?): Boolean {
-    return headers != null && headers.isNotEmpty()
-  }
+    companion object {
 
-  companion object {
-    private const val TIMESTAMP_FORMAT = "MM/dd/yyyy HH:mm:ss"
-    private const val RANGE_START_HTTP_OK = 200
-    private const val RANGE_END_HTTP_OK = 299
-    private const val RANGE_END_HTTP_REDIRECTION = 399
-  }
+        private const val TIMESTAMP_FORMAT =
+            "MM/dd/yyyy HH:mm:ss"
+
+        private const val RANGE_START_HTTP_OK =
+            200
+
+        private const val RANGE_END_HTTP_OK =
+            299
+
+        private const val RANGE_END_HTTP_REDIRECTION =
+            399
+    }
 }
