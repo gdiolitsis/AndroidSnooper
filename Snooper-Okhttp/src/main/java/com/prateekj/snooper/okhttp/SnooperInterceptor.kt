4ae0142590b81c181e2d1c6f9eb42087db21754f
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 11:31:17 +0300

    Update SnooperInterceptor.kt

--- Snooper-Okhttp/src/main/java/com/prateekj/snooper/okhttp/SnooperInterceptor.kt
+++ Snooper-Okhttp/src/main/java/com/prateekj/snooper/okhttp/SnooperInterceptor.kt
@@ -3,74 +3,147 @@ package com.prateekj.snooper.okhttp
 import com.prateekj.snooper.AndroidSnooper
 import com.prateekj.snooper.networksnooper.model.HttpCall
 import com.prateekj.snooper.utils.Logger
-
-import java.io.IOException
-import java.util.HashMap
-
 import okhttp3.Headers
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
 import okio.Buffer
-
-import okhttp3.ResponseBody.create
+import java.io.IOException
 
 class SnooperInterceptor : Interceptor {
 
-  @Throws(IOException::class)
-  override fun intercept(chain: Interceptor.Chain): Response {
-    val response: Response
-    val responseBody: String
-    val request = chain.request()
-    val builder = HttpCall.Builder()
-      .withUrl(request.url().toString())
-      .withPayload(getRequestBody(request))
-      .withMethod(request.method())
-      .withRequestHeaders(headers(request.headers()))
-    try {
-      response = chain.proceed(request)
-      responseBody = response.body().string()
-    } catch (e: Exception) {
-      val httpCall = builder.withError(e.toString()).build()
-      AndroidSnooper.instance.record(httpCall)
-      throw e
+    @Throws(IOException::class)
+    override fun intercept(
+        chain: Interceptor.Chain
+    ): Response {
+
+        val request = chain.request()
+
+        val builder = HttpCall.Builder()
+
+            .withUrl(request.url.toString())
+
+            .withPayload(getRequestBody(request))
+
+            .withMethod(request.method)
+
+            .withRequestHeaders(
+                headers(request.headers)
+            )
+
+        return try {
+
+            val response =
+                chain.proceed(request)
+
+            val responseBodyString = try {
+
+                response.body
+                    ?.string()
+                    ?: ""
+
+            } catch (_: Throwable) {
+                ""
+            }
+
+            val httpCall = builder
+
+                .withResponseBody(
+                    responseBodyString
+                )
+
+                .withStatusCode(
+                    response.code
+                )
+
+                .withStatusText(
+                    response.message
+                )
+
+                .withResponseHeaders(
+                    headers(response.headers)
+                )
+
+                .build()
+
+            try {
+                AndroidSnooper.instance.record(httpCall)
+            } catch (_: Throwable) {}
+
+            response.newBuilder()
+
+                .body(
+                    responseBodyString.toResponseBody(
+                        response.body?.contentType()
+                    )
+                )
+
+                .build()
+
+        } catch (e: Exception) {
+
+            val httpCall = builder
+
+                .withError(
+                    e.toString()
+                )
+
+                .build()
+
+            try {
+                AndroidSnooper.instance.record(httpCall)
+            } catch (_: Throwable) {}
+
+            throw e
+        }
     }
 
-    val httpCall = builder.withResponseBody(responseBody)
-      .withStatusCode(response.code())
-      .withStatusText(response.message())
-      .withResponseHeaders(headers(response.headers())).build()
-    AndroidSnooper.instance.record(httpCall)
-    return response.newBuilder().body(create(response.body().contentType(), responseBody))
-      .build()
-  }
+    private fun headers(
+        headers: Headers
+    ): Map<String, List<String>> {
 
-  private fun headers(headers: Headers): Map<String, List<String>> {
-    val extractedHeaders = HashMap<String, List<String>>()
-    for (headerName in headers.names()) {
-      extractedHeaders[headerName] = headers.values(headerName)
-    }
-    return extractedHeaders
-  }
+        val extractedHeaders =
+            HashMap<String, List<String>>()
 
-  private fun getRequestBody(request: Request): String {
-    try {
-      val copy = request.newBuilder().build()
-      val buffer = Buffer()
-      if (copy.body() == null) {
-        return ""
-      }
-      copy.body().writeTo(buffer)
-      return buffer.readUtf8()
-    } catch (e: IOException) {
-      Logger.e(TAG, "couldn't retrieve request body", e)
-      return ""
+        for (headerName in headers.names()) {
+
+            extractedHeaders[headerName] =
+                headers.values(headerName)
+        }
+
+        return extractedHeaders
     }
 
-  }
+    private fun getRequestBody(
+        request: Request
+    ): String {
 
-  companion object {
+        return try {
 
-    val TAG = SnooperInterceptor::class.java.simpleName
-  }
+            val body = request.body ?: return ""
+
+            val buffer = Buffer()
+
+            body.writeTo(buffer)
+
+            buffer.readUtf8()
+
+        } catch (e: IOException) {
+
+            Logger.e(
+                TAG,
+                "Could not retrieve request body",
+                e
+            )
+
+            ""
+        }
+    }
+
+    companion object {
+
+        private val TAG =
+            SnooperInterceptor::class.java.simpleName
+    }
 }
