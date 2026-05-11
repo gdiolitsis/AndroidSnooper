169eed607dffa626ada8ceab82d46c35af01b5e4
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:22:18 +0300

    Update NonScrollListView.kt

--- Snooper/src/main/java/com/prateekj/snooper/customviews/NonScrollListView.kt
+++ Snooper/src/main/java/com/prateekj/snooper/customviews/NonScrollListView.kt
@@ -7,30 +7,64 @@ import android.util.AttributeSet
 import android.widget.ListView
 
 class NonScrollListView : ListView {
-  constructor(context: Context) : super(context) {}
 
-  constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {}
+    constructor(
+        context: Context
+    ) : super(context)
 
-  constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
-    context,
-    attrs,
-    defStyleAttr
-  )
-
-  @TargetApi(LOLLIPOP)
-  constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(
-    context,
-    attrs,
-    defStyleAttr,
-    defStyleRes
-  )
-
-  override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-    val heightMeasureSpecCustom = MeasureSpec.makeMeasureSpec(
-      Integer.MAX_VALUE shr 2, MeasureSpec.AT_MOST
+    constructor(
+        context: Context,
+        attrs: AttributeSet
+    ) : super(
+        context,
+        attrs
     )
-    super.onMeasure(widthMeasureSpec, heightMeasureSpecCustom)
-    val params = layoutParams
-    params.height = measuredHeight
-  }
+
+    constructor(
+        context: Context,
+        attrs: AttributeSet,
+        defStyleAttr: Int
+    ) : super(
+        context,
+        attrs,
+        defStyleAttr
+    )
+
+    @TargetApi(LOLLIPOP)
+    constructor(
+        context: Context,
+        attrs: AttributeSet,
+        defStyleAttr: Int,
+        defStyleRes: Int
+    ) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes
+    )
+
+    override fun onMeasure(
+        widthMeasureSpec: Int,
+        heightMeasureSpec: Int
+    ) {
+
+        val expandedHeightSpec =
+            MeasureSpec.makeMeasureSpec(
+                Int.MAX_VALUE shr 2,
+                MeasureSpec.AT_MOST
+            )
+
+        super.onMeasure(
+            widthMeasureSpec,
+            expandedHeightSpec
+        )
+
+        layoutParams?.let { params ->
+
+            params.height =
+                measuredHeight
+
+            layoutParams = params
+        }
+    }
 }
