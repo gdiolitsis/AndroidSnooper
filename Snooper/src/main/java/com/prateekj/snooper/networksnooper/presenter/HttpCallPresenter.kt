533ee9f9676d8f066afbed4edfce5bcd8bd9e50d
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:46:22 +0300

    Update HttpCallPresenter.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/presenter/HttpCallPresenter.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/presenter/HttpCallPresenter.kt
@@ -10,55 +10,119 @@ import com.prateekj.snooper.networksnooper.helper.DataCopyHelper
 import com.prateekj.snooper.networksnooper.model.HttpCallRecord
 import com.prateekj.snooper.networksnooper.views.HttpCallView
 import com.prateekj.snooper.utils.FileUtil
-import java.lang.String.format
 import java.text.SimpleDateFormat
 import java.util.Locale.US
 
 class HttpCallPresenter(
-  private val dataCopyHelper: DataCopyHelper,
-  private val httpCallRecord: HttpCallRecord,
-  private val view: HttpCallView,
-  private val fileUtil: FileUtil,
-  private val executor: BackgroundTaskExecutor
+    private val dataCopyHelper:
+            DataCopyHelper,
+    private val httpCallRecord:
+            HttpCallRecord,
+    private val view:
+            HttpCallView,
+    private val fileUtil:
+            FileUtil,
+    private val executor:
+            BackgroundTaskExecutor
 ) {
 
-  private val logFileName: String
-    get() {
-      val df = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", US)
-      return format("%s.txt", df.format(httpCallRecord.date))
-    }
+    private val logFileName:
+            String
+        get() {
 
-  fun copyHttpCallBody(httpCallTab: HttpCallTab) {
-    val text = getTextToCopy(httpCallTab)
-    view.copyToClipboard(text)
-  }
+            val dateFormat =
+                SimpleDateFormat(
+                    "yyyy_MM_dd_HH_mm_ss",
+                    US
+                )
 
-  fun shareHttpCallBody() {
-    val completeHttpCallData = dataCopyHelper.getHttpCallData()
-    val logFileName = logFileName
-    executor.execute(object : BackgroundTask<String> {
-      override fun onExecute(): String {
-        return fileUtil.createLogFile(completeHttpCallData, logFileName)
-      }
+            val date =
+                httpCallRecord.date
 
-      override fun onResult(result: String) {
-        if (result.isNotEmpty()) {
-          view.shareData(result)
+            return "${dateFormat.format(date)}.txt"
         }
-      }
-    })
-  }
 
-  fun onPermissionDenied() {
-    view.showMessageShareNotAvailable()
-  }
+    fun copyHttpCallBody(
+        httpCallTab: HttpCallTab
+    ) {
 
-  private fun getTextToCopy(httpCallTab: HttpCallTab): String {
-    return when (httpCallTab) {
-      RESPONSE -> dataCopyHelper.getResponseDataForCopy()
-      REQUEST -> dataCopyHelper.getRequestDataForCopy()
-      HEADERS -> dataCopyHelper.getHeadersForCopy()
-      else -> dataCopyHelper.getErrorsForCopy() ?: ""
+        val text =
+            getTextToCopy(
+                httpCallTab
+            )
+
+        view.copyToClipboard(text)
     }
-  }
+
+    fun shareHttpCallBody() {
+
+        val completeHttpCallData =
+            dataCopyHelper.getHttpCallData()
+
+        val fileName =
+            logFileName
+
+        executor.execute(
+            object : BackgroundTask<String> {
+
+                override fun onExecute():
+                        String {
+
+                    return fileUtil.createLogFile(
+                        completeHttpCallData,
+                        fileName
+                    )
+                }
+
+                override fun onResult(
+                    result: String
+                ) {
+
+                    if (
+                        result.isNotEmpty()
+                    ) {
+
+                        view.shareData(result)
+                    }
+                }
+            }
+        )
+    }
+
+    fun onPermissionDenied() {
+
+        view.showMessageShareNotAvailable()
+    }
+
+    private fun getTextToCopy(
+        httpCallTab: HttpCallTab
+    ): String {
+
+        return when (httpCallTab) {
+
+            RESPONSE -> {
+
+                dataCopyHelper
+                    .getResponseDataForCopy()
+            }
+
+            REQUEST -> {
+
+                dataCopyHelper
+                    .getRequestDataForCopy()
+            }
+
+            HEADERS -> {
+
+                dataCopyHelper
+                    .getHeadersForCopy()
+            }
+
+            else -> {
+
+                dataCopyHelper
+                    .getErrorsForCopy()
+            }
+        }
+    }
 }
