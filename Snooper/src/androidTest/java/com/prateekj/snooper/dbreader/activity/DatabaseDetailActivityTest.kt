90ff6acb069a3d56391fd92499b6c8e103a8c8d3
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 11:48:28 +0300

    Update DatabaseDetailActivityTest.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/dbreader/activity/DatabaseDetailActivityTest.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/dbreader/activity/DatabaseDetailActivityTest.kt
@@ -2,16 +2,6 @@ package com.prateekj.snooper.dbreader.activity
 
 import android.content.Intent
 import android.view.View
-
-import androidx.test.espresso.intent.rule.IntentsTestRule
-
-import com.prateekj.snooper.R
-import com.prateekj.snooper.rules.TestDbRule
-
-import org.hamcrest.CoreMatchers
-import org.junit.Rule
-import org.junit.Test
-
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -21,51 +11,148 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import com.prateekj.snooper.R
 import com.prateekj.snooper.dbreader.activity.DatabaseDetailActivity.Companion.TABLE_NAME
 import com.prateekj.snooper.dbreader.activity.DatabaseListActivity.Companion.DB_PATH
+import com.prateekj.snooper.rules.TestDbRule
 import com.prateekj.snooper.utils.EspressoViewMatchers.withRecyclerView
-import org.hamcrest.core.AllOf.allOf
+import org.hamcrest.CoreMatchers.allOf
+import org.junit.Rule
+import org.junit.Test
 
 class DatabaseDetailActivityTest {
-  @get:Rule
-  var testDbRule = TestDbRule(R.raw.test, "test.db")
 
-  @get:Rule
-  var activityRule = IntentsTestRule(DatabaseDetailActivity::class.java, true, false)
+    @get:Rule
+    var testDbRule =
+        TestDbRule(
+            R.raw.test,
+            "test.db"
+        )
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldRenderDatabaseInformation() {
-    val intent = Intent()
-    val dbPath = testDbRule.dbDirectory + "/test.db"
-    intent.putExtra(DatabaseDetailActivity.DB_PATH, dbPath)
-    intent.putExtra(DatabaseListActivity.DB_NAME, "test.db")
-    activityRule.launchActivity(intent)
-    onView(withId(R.id.db_name)).check(matches(withText("test.db")))
-    onView(withId(R.id.db_version)).check(matches(withText("0")))
-    onView(withRecyclerView(R.id.table_list, 0)).check(
-      matches(
-        allOf<View>(
-          hasDescendant(withText("1. ")),
-          hasDescendant(withText("person"))
+    @get:Rule
+    var activityRule =
+        ActivityScenarioRule(
+            DatabaseDetailActivity::class.java
         )
-      )
-    )
-    onView(withRecyclerView(R.id.table_list, 1)).check(
-      matches(
-        allOf<View>(
-          hasDescendant(withText("2. ")),
-          hasDescendant(withText("sqlite_sequence"))
+
+    @Test
+    @Throws(Exception::class)
+    fun shouldRenderDatabaseInformation() {
+
+        val context =
+            InstrumentationRegistry
+                .getInstrumentation()
+                .targetContext
+
+        val dbPath =
+            "${testDbRule.dbDirectory}/test.db"
+
+        val intent = Intent(
+            context,
+            DatabaseDetailActivity::class.java
+        ).apply {
+
+            putExtra(
+                DatabaseDetailActivity.DB_PATH,
+                dbPath
+            )
+
+            putExtra(
+                DatabaseListActivity.DB_NAME,
+                "test.db"
+            )
+        }
+
+        activityRule.scenario.onActivity {
+
+            it.intent.putExtras(intent)
+        }
+
+        onView(withId(R.id.db_name))
+            .check(
+                matches(
+                    withText("test.db")
+                )
+            )
+
+        onView(withId(R.id.db_version))
+            .check(
+                matches(
+                    withText("0")
+                )
+            )
+
+        onView(
+            withRecyclerView(
+                R.id.table_list,
+                0
+            )
+        ).check(
+
+            matches(
+
+                allOf<View>(
+
+                    hasDescendant(
+                        withText("1. ")
+                    ),
+
+                    hasDescendant(
+                        withText("person")
+                    )
+                )
+            )
         )
-      )
-    )
-    onView(withRecyclerView(R.id.table_list, 0)).perform(click())
-    intended(
-      CoreMatchers.allOf(
-        hasComponent(TableDetailActivity::class.java.name),
-        hasExtra(TABLE_NAME, "person"),
-        hasExtra(DB_PATH, dbPath)
-      )
-    )
-  }
+
+        onView(
+            withRecyclerView(
+                R.id.table_list,
+                1
+            )
+        ).check(
+
+            matches(
+
+                allOf<View>(
+
+                    hasDescendant(
+                        withText("2. ")
+                    ),
+
+                    hasDescendant(
+                        withText("sqlite_sequence")
+                    )
+                )
+            )
+        )
+
+        onView(
+            withRecyclerView(
+                R.id.table_list,
+                0
+            )
+        ).perform(click())
+
+        intended(
+
+            allOf(
+
+                hasComponent(
+                    TableDetailActivity::class.java.name
+                ),
+
+                hasExtra(
+                    TABLE_NAME,
+                    "person"
+                ),
+
+                hasExtra(
+                    DB_PATH,
+                    dbPath
+                )
+            )
+        )
+    }
 }
