ea7e9366e3c231fa52733e7a4c2b49fc5009cacc
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:35:02 +0300

    Update TableAdapter.kt

--- Snooper/src/main/java/com/prateekj/snooper/dbreader/adapter/TableAdapter.kt
+++ Snooper/src/main/java/com/prateekj/snooper/dbreader/adapter/TableAdapter.kt
@@ -8,33 +8,67 @@ import com.prateekj.snooper.R
 import kotlinx.android.synthetic.main.table_item.view.*
 
 class TableAdapter(
-  private val tableList: List<String>,
-  private val tableEventListener: TableEventListener
+    private val tableList: List<String>,
+    private val tableEventListener: TableEventListener
 ) : RecyclerView.Adapter<TableAdapter.TableViewHolder>() {
 
-  class TableViewHolder(
-    val view: View,
-    private val tableEventListener: TableEventListener
-  ) : RecyclerView.ViewHolder(view) {
+    class TableViewHolder(
+        private val view: View,
+        private val tableEventListener: TableEventListener
+    ) : RecyclerView.ViewHolder(view) {
 
-    fun bind(tableName: String, rowNum: Int) {
-      view.table_name.text = tableName
-      view.row_num.text = "$rowNum. "
-      this.itemView.setOnClickListener { tableEventListener.onTableClick(tableName) }
+        fun bind(
+            tableName: String,
+            rowNum: Int
+        ) {
+
+            view.table_name?.text =
+                tableName
+
+            view.row_num?.text =
+                "$rowNum. "
+
+            itemView.setOnClickListener {
+
+                tableEventListener.onTableClick(
+                    tableName
+                )
+            }
+        }
     }
-  }
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TableViewHolder {
-    val itemView = LayoutInflater.from(parent.context)
-      .inflate(R.layout.table_item, parent, false)
-    return TableViewHolder(itemView, tableEventListener)
-  }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TableViewHolder {
 
-  override fun onBindViewHolder(holder: TableViewHolder, position: Int) {
-    holder.bind(tableList[position], position + 1)
-  }
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(
+                    R.layout.table_item,
+                    parent,
+                    false
+                )
 
-  override fun getItemCount(): Int {
-    return tableList.size
-  }
+        return TableViewHolder(
+            itemView,
+            tableEventListener
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: TableViewHolder,
+        position: Int
+    ) {
+
+        holder.bind(
+            tableList[position],
+            position + 1
+        )
+    }
+
+    override fun getItemCount(): Int {
+
+        return tableList.size
+    }
 }
