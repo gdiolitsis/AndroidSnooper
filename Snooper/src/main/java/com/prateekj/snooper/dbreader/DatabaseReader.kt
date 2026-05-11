34d6440776bf0f453bbe4504a4331f23ae2aa909
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:28:05 +0300

    Update DatabaseReader.kt

--- Snooper/src/main/java/com/prateekj/snooper/dbreader/DatabaseReader.kt
+++ Snooper/src/main/java/com/prateekj/snooper/dbreader/DatabaseReader.kt
@@ -3,7 +3,6 @@ package com.prateekj.snooper.dbreader
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
-
 import com.prateekj.snooper.dbreader.model.Database
 import com.prateekj.snooper.dbreader.model.Table
 import com.prateekj.snooper.dbreader.tasks.DatabaseListBackgroundTask
@@ -15,68 +14,143 @@ import com.prateekj.snooper.infra.BackgroundTaskExecutor
 import com.prateekj.snooper.utils.Logger
 
 class DatabaseReader(
-  private val context: Context,
-  private val executor: BackgroundTaskExecutor,
-  private val databaseDataReader: DatabaseDataReader
+    private val context: Context,
+    private val executor: BackgroundTaskExecutor,
+    private val databaseDataReader: DatabaseDataReader
 ) {
 
-  fun fetchApplicationDatabases(dbReaderCallback: DbReaderCallback) {
-    dbReaderCallback.onDbFetchStarted()
-    executor.execute(DatabaseListBackgroundTask(context, dbReaderCallback))
-  }
+    fun fetchApplicationDatabases(
+        dbReaderCallback: DbReaderCallback
+    ) {
 
-  fun fetchDbContent(dbViewCallback: DbViewCallback, dbPath: String, dbName: String) {
-    dbViewCallback.onDbFetchStarted()
-    executor.execute(object : BackgroundTask<Database?> {
-      override fun onExecute(): Database? {
-        val database = getDatabase(dbPath)
-        if (database != null) {
-          val dbWithData = databaseDataReader.getData(database)
-          dbWithData.name = dbName
-          return dbWithData
-        }
-        return null
-      }
+        dbReaderCallback.onDbFetchStarted()
 
-      override fun onResult(result: Database?) {
-        if (result != null) {
-          dbViewCallback.onDbFetchCompleted(result)
-        }
-      }
-    })
-  }
-
-  fun fetchTableContent(tableViewCallback: TableViewCallback, dbPath: String, tableName: String) {
-    tableViewCallback.onTableFetchStarted()
-    executor.execute(object : BackgroundTask<Table?> {
-      override fun onExecute(): Table? {
-        val database = getDatabase(dbPath)
-        return if (database != null) {
-          databaseDataReader.getTableData(database, tableName)
-        } else null
-      }
-
-      override fun onResult(result: Table?) {
-        if (result != null) {
-          tableViewCallback.onTableFetchCompleted(result)
-        }
-      }
-    })
-  }
-
-
-  private fun getDatabase(path: String): SQLiteDatabase? {
-    var sqLiteDatabase: SQLiteDatabase? = null
-    try {
-      sqLiteDatabase = SQLiteDatabase.openDatabase(path, null, SQLiteDatabase.OPEN_READONLY)
-    } catch (exception: SQLiteException) {
-      Logger.e(TAG, "Exception while opening the database", exception)
+        executor.execute(
+            DatabaseListBackgroundTask(
+                context,
+                dbReaderCallback
+            )
+        )
     }
 
-    return sqLiteDatabase
-  }
+    fun fetchDbContent(
+        dbViewCallback: DbViewCallback,
+        dbPath: String,
+        dbName: String
+    ) {
 
-  companion object {
-    private val TAG = DatabaseReader::class.java.name
-  }
+        dbViewCallback.onDbFetchStarted()
+
+        executor.execute(
+            object : BackgroundTask<Database?> {
+
+                override fun onExecute(): Database? {
+
+                    val database =
+                        getDatabase(dbPath)
+                            ?: return null
+
+                    return try {
+
+                        databaseDataReader
+                            .getData(database)
+                            .apply {
+
+                                name = dbName
+                            }
+
+                    } finally {
+
+                        database.close()
+                    }
+                }
+
+                override fun onResult(
+                    result: Database?
+                ) {
+
+                    result?.let {
+
+                        dbViewCallback
+                            .onDbFetchCompleted(it)
+                    }
+                }
+            }
+        )
+    }
+
+    fun fetchTableContent(
+        tableViewCallback: TableViewCallback,
+        dbPath: String,
+        tableName: String
+    ) {
+
+        tableViewCallback.onTableFetchStarted()
+
+        executor.execute(
+            object : BackgroundTask<Table?> {
+
+                override fun onExecute(): Table? {
+
+                    val database =
+                        getDatabase(dbPath)
+                            ?: return null
+
+                    return try {
+
+                        databaseDataReader
+                            .getTableData(
+                                database,
+                                tableName
+                            )
+
+                    } finally {
+
+                        database.close()
+                    }
+                }
+
+                override fun onResult(
+                    result: Table?
+                ) {
+
+                    result?.let {
+
+                        tableViewCallback
+                            .onTableFetchCompleted(it)
+                    }
+                }
+            }
+        )
+    }
+
+    private fun getDatabase(
+        path: String
+    ): SQLiteDatabase? {
+
+        return try {
+
+            SQLiteDatabase.openDatabase(
+                path,
+                null,
+                SQLiteDatabase.OPEN_READONLY
+            )
+
+        } catch (exception: SQLiteException) {
+
+            Logger.e(
+                TAG,
+                "Exception while opening the database",
+                exception
+            )
+
+            null
+        }
+    }
+
+    companion object {
+
+        private val TAG =
+            DatabaseReader::class.java.name
+    }
 }
