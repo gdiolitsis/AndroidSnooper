6c1440cc948006c967de5873383fceb17e46af5b
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 02:09:05 +0300

    Update HttpCallBodyView.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/views/HttpCallBodyView.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/views/HttpCallBodyView.kt
@@ -1,10 +1,14 @@
 package com.prateekj.snooper.networksnooper.views
 
-
 import com.prateekj.snooper.networksnooper.model.Bound
 
 interface HttpCallBodyView {
-  fun onFormattingDone()
-  fun highlightBounds(bounds: List<Bound>)
-  fun removeOldHighlightedSpans()
+
+    fun onFormattingDone()
+
+    fun highlightBounds(
+        bounds: List<Bound>
+    )
+
+    fun removeOldHighlightedSpans()
 }
