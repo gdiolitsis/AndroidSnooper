0a8d1c2bc34d56b8765d66cfc0a1466d4420d8d1
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 02:11:26 +0300

    Update HttpListView.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/views/HttpListView.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/views/HttpListView.kt
@@ -3,11 +3,24 @@ package com.prateekj.snooper.networksnooper.views
 import com.prateekj.snooper.networksnooper.model.HttpCallRecord
 
 interface HttpListView {
-  fun navigateToResponseBody(httpCallId: Long)
-  fun finishView()
-  fun showDeleteConfirmationDialog()
-  fun updateListViewAfterDelete()
-  fun initHttpCallRecordList(httpCallRecords: List<HttpCallRecord>)
-  fun appendRecordList(httpCallRecords: List<HttpCallRecord>)
-  fun renderNoCallsFoundView()
+
+    fun navigateToResponseBody(
+        httpCallId: Long
+    )
+
+    fun finishView()
+
+    fun showDeleteConfirmationDialog()
+
+    fun updateListViewAfterDelete()
+
+    fun initHttpCallRecordList(
+        httpCallRecords: List<HttpCallRecord>
+    )
+
+    fun appendRecordList(
+        httpCallRecords: List<HttpCallRecord>
+    )
+
+    fun renderNoCallsFoundView()
 }
