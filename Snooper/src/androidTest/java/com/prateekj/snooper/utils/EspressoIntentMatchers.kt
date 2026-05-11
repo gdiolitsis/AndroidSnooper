affd5ca9bd77a066a549582db83e2f3129194427
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:06:44 +0300

    Update EspressoIntentMatchers.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/utils/EspressoIntentMatchers.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/utils/EspressoIntentMatchers.kt
@@ -3,34 +3,56 @@ package com.prateekj.snooper.utils
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-
 import org.hamcrest.CustomTypeSafeMatcher
 import org.hamcrest.Matcher
-
 import android.content.Intent.EXTRA_INTENT
 import android.content.Intent.EXTRA_STREAM
 import android.content.Intent.EXTRA_SUBJECT
 
 object EspressoIntentMatchers {
 
-  fun forMailChooserIntent(
-    action: String,
-    mimeType: String,
-    extraData: String,
-    fileName: String
-  ): Matcher<Bundle> {
-    return object :
-      CustomTypeSafeMatcher<Bundle>("Custom matcher for matching mail chooser intent") {
-      override fun matchesSafely(item: Bundle): Boolean {
-        val intent = item.get(EXTRA_INTENT) as Intent
-        val uri = intent.getParcelableExtra<Uri>(EXTRA_STREAM)
+    fun forMailChooserIntent(
+        action: String,
+        mimeType: String,
+        extraData: String,
+        fileName: String
+    ): Matcher<Bundle> {
 
-        return action == intent.action &&
-          mimeType == intent.type &&
-          intent.getStringExtra(EXTRA_SUBJECT).equals(extraData, ignoreCase = true) &&
-          uri.path!!.endsWith(fileName)
-      }
+        return object : CustomTypeSafeMatcher<Bundle>(
+            "Custom matcher for matching mail chooser intent"
+        ) {
+
+            override fun matchesSafely(
+                item: Bundle
+            ): Boolean {
+
+                val intent =
+                    item.get(
+                        EXTRA_INTENT
+                    ) as? Intent
+                        ?: return false
+
+                val uri =
+                    intent.getParcelableExtra<Uri>(
+                        EXTRA_STREAM
+                    ) ?: return false
+
+                val uriPath =
+                    uri.path ?: return false
+
+                val subject =
+                    intent.getStringExtra(
+                        EXTRA_SUBJECT
+                    ) ?: return false
+
+                return action == intent.action &&
+                        mimeType == intent.type &&
+                        subject.equals(
+                            extraData,
+                            ignoreCase = true
+                        ) &&
+                        uriPath.endsWith(fileName)
+            }
+        }
     }
-  }
-
 }
