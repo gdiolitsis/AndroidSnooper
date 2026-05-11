da15ba72bab2f73d6d4854e081b9aa82455fe07e
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:51:29 +0300

    Update FileUtil.kt

--- Snooper/src/main/java/com/prateekj/snooper/utils/FileUtil.kt
+++ Snooper/src/main/java/com/prateekj/snooper/utils/FileUtil.kt
@@ -1,39 +1,63 @@
 package com.prateekj.snooper.utils
 
-import android.os.Environment
-
+import android.content.Context
 import java.io.File
-import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.OutputStreamWriter
 
-class FileUtil {
+class FileUtil(
+    private val context: Context
+) {
 
-  fun createLogFile(content: StringBuilder, fileName: String): String {
-    var filePath = ""
-    try {
-      val file = File(Environment.getExternalStorageDirectory().toString() + "/" + fileName)
-      if (file.exists()) {
-        file.delete()
-      }
-      file.createNewFile()
+    fun createLogFile(
+        content: StringBuilder,
+        fileName: String
+    ): String {
 
-      FileOutputStream(file).use { fos ->
-        OutputStreamWriter(fos).use { osw ->
-          osw.append(content.toString())
+        return try {
+
+            val file =
+                File(
+                    context.cacheDir,
+                    fileName
+                )
+
+            if (file.exists()) {
+                file.delete()
+            }
+
+            file.createNewFile()
+
+            FileOutputStream(file).use { fos ->
+
+                OutputStreamWriter(fos).use { writer ->
+
+                    writer.append(
+                        content.toString()
+                    )
+
+                    writer.flush()
+                }
+            }
+
+            file.absolutePath
+
+        } catch (e: IOException) {
+
+            Logger.e(
+                TAG,
+                "Error while creating log file",
+                e
+            )
+
+            ""
         }
-      }
-      filePath = file.absolutePath
-    } catch (e: FileNotFoundException) {
-      Logger.e(TAG, "File Not Found Exception occurred while closing the output stream", e)
-    } catch (e: IOException) {
-      Logger.e(TAG, "IO Exception occurred while closing the output stream", e)
     }
-    return filePath
-  }
 
-  companion object {
-    private val TAG = FileUtil::class.java.simpleName
-  }
+    companion object {
+
+        private val TAG =
+            FileUtil::class.java.simpleName
+    }
 }
