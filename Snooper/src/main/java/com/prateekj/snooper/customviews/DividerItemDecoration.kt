0551942e4b4c1e13edb449e851d8e6a34a61ae28
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:20:43 +0300

    Update DividerItemDecoration.kt

--- Snooper/src/main/java/com/prateekj/snooper/customviews/DividerItemDecoration.kt
+++ Snooper/src/main/java/com/prateekj/snooper/customviews/DividerItemDecoration.kt
@@ -9,75 +9,185 @@ import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class DividerItemDecoration(context: Context, orientation: Int, drawableId: Int) :
-  RecyclerView.ItemDecoration() {
-  private val mDivider: Drawable? = ContextCompat.getDrawable(context, drawableId)
-  private var mOrientation: Int = 0
+class DividerItemDecoration(
+    context: Context,
+    orientation: Int,
+    drawableId: Int
+) : RecyclerView.ItemDecoration() {
 
-  init {
-    setOrientation(orientation)
-  }
+    private val divider: Drawable? =
+        ContextCompat.getDrawable(
+            context,
+            drawableId
+        )
 
-  fun setOrientation(orientation: Int) {
-    require(!(orientation != HORIZONTAL && orientation != VERTICAL)) { "invalid orientation" }
-    mOrientation = orientation
-  }
+    private var orientation =
+        VERTICAL
 
-  override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-    if (mOrientation == VERTICAL) {
-      drawVertical(c, parent)
-    } else {
-      drawHorizontal(c, parent)
+    init {
+
+        setOrientation(orientation)
     }
-  }
 
-  private fun drawVertical(c: Canvas, parent: RecyclerView) {
-    val left = parent.paddingLeft
-    val right = parent.width - parent.paddingRight
+    fun setOrientation(
+        orientation: Int
+    ) {
 
-    val childCount = parent.childCount
-    for (i in 0 until childCount) {
-      val child = parent.getChildAt(i)
-      val params = child
-        .layoutParams as RecyclerView.LayoutParams
-      val top = child.bottom + params.bottomMargin
-      val bottom = top + mDivider!!.intrinsicHeight
-      mDivider.setBounds(left, top, right, bottom)
-      mDivider.draw(c)
+        require(
+            orientation == HORIZONTAL ||
+            orientation == VERTICAL
+        ) {
+            "Invalid orientation"
+        }
+
+        this.orientation = orientation
     }
-  }
 
-  private fun drawHorizontal(c: Canvas, parent: RecyclerView) {
-    val top = parent.paddingTop
-    val bottom = parent.height - parent.paddingBottom
+    override fun onDrawOver(
+        canvas: Canvas,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
 
-    val childCount = parent.childCount
-    for (i in 0 until childCount) {
-      val child = parent.getChildAt(i)
-      val params = child
-        .layoutParams as RecyclerView.LayoutParams
-      val left = child.right + params.rightMargin
-      val right = left + mDivider!!.intrinsicHeight
-      mDivider.setBounds(left, top, right, bottom)
-      mDivider.draw(c)
+        val drawable =
+            divider ?: return
+
+        if (orientation == VERTICAL) {
+
+            drawVertical(
+                canvas,
+                parent,
+                drawable
+            )
+
+        } else {
+
+            drawHorizontal(
+                canvas,
+                parent,
+                drawable
+            )
+        }
     }
-  }
 
-  override fun getItemOffsets(
-    outRect: Rect,
-    view: View,
-    parent: RecyclerView,
-    state: RecyclerView.State
-  ) {
-    if (mOrientation == VERTICAL) {
-      outRect.set(0, 0, 0, mDivider!!.intrinsicHeight)
-    } else {
-      outRect.set(0, 0, mDivider!!.intrinsicWidth, 0)
+    private fun drawVertical(
+        canvas: Canvas,
+        parent: RecyclerView,
+        drawable: Drawable
+    ) {
+
+        val left =
+            parent.paddingLeft
+
+        val right =
+            parent.width - parent.paddingRight
+
+        val childCount =
+            parent.childCount
+
+        for (i in 0 until childCount) {
+
+            val child =
+                parent.getChildAt(i)
+
+            val params =
+                child.layoutParams
+                        as RecyclerView.LayoutParams
+
+            val top =
+                child.bottom + params.bottomMargin
+
+            val bottom =
+                top + drawable.intrinsicHeight
+
+            drawable.setBounds(
+                left,
+                top,
+                right,
+                bottom
+            )
+
+            drawable.draw(canvas)
+        }
     }
-  }
 
-  companion object {
-    const val HORIZONTAL = LinearLayoutManager.HORIZONTAL
-    const val VERTICAL = LinearLayoutManager.VERTICAL
-  }
+    private fun drawHorizontal(
+        canvas: Canvas,
+        parent: RecyclerView,
+        drawable: Drawable
+    ) {
+
+        val top =
+            parent.paddingTop
+
+        val bottom =
+            parent.height - parent.paddingBottom
+
+        val childCount =
+            parent.childCount
+
+        for (i in 0 until childCount) {
+
+            val child =
+                parent.getChildAt(i)
+
+            val params =
+                child.layoutParams
+                        as RecyclerView.LayoutParams
+
+            val left =
+                child.right + params.rightMargin
+
+            val right =
+                left + drawable.intrinsicWidth
+
+            drawable.setBounds(
+                left,
+                top,
+                right,
+                bottom
+            )
+
+            drawable.draw(canvas)
+        }
+    }
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+
+        val drawable =
+            divider ?: return
+
+        if (orientation == VERTICAL) {
+
+            outRect.set(
+                0,
+                0,
+                0,
+                drawable.intrinsicHeight
+            )
+
+        } else {
+
+            outRect.set(
+                0,
+                0,
+                drawable.intrinsicWidth,
+                0
+            )
+        }
+    }
+
+    companion object {
+
+        const val HORIZONTAL =
+            LinearLayoutManager.HORIZONTAL
+
+        const val VERTICAL =
+            LinearLayoutManager.VERTICAL
+    }
 }
