ee93b12b9d4a7437c2ae6949b4bf6e8ec281c81e
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 02:23:43 +0300

    Update HttpCallFragmentPresenterTest.kt

--- Snooper/src/test/java/com/prateekj/snooper/networksnooper/presenter/HttpCallFragmentPresenterTest.kt
+++ Snooper/src/test/java/com/prateekj/snooper/networksnooper/presenter/HttpCallFragmentPresenterTest.kt
@@ -24,214 +24,552 @@ import org.junit.Test
 
 class HttpCallFragmentPresenterTest {
 
-  private lateinit var presenter: HttpCallFragmentPresenter
-  private lateinit var repo: SnooperRepo
-  private lateinit var viewModel: HttpBodyViewModel
-  private lateinit var httpCallRecord: HttpCallRecord
-  private lateinit var factory: ResponseFormatterFactory
-  private lateinit var responseFormatter: ResponseFormatter
-  private lateinit var responseBody: String
-  private lateinit var requestPayload: String
-  private lateinit var error: String
-  private lateinit var formattedBody: String
-  private lateinit var mockExecutor: BackgroundTaskExecutor
-  private lateinit var httpCallBodyView: HttpCallBodyView
+    private lateinit var presenter:
+            HttpCallFragmentPresenter
 
-  private val jsonContentTypeHeader: HttpHeader
-    get() {
-      val headerValue = HttpHeaderValue("application/json")
-      val httpHeader = HttpHeader("Content-Type")
-      httpHeader.values = listOf(headerValue)
-      return httpHeader
+    private lateinit var repo:
+            SnooperRepo
+
+    private lateinit var viewModel:
+            HttpBodyViewModel
+
+    private lateinit var httpCallRecord:
+            HttpCallRecord
+
+    private lateinit var factory:
+            ResponseFormatterFactory
+
+    private lateinit var responseFormatter:
+            ResponseFormatter
+
+    private lateinit var responseBody:
+            String
+
+    private lateinit var requestPayload:
+            String
+
+    private lateinit var error:
+            String
+
+    private lateinit var formattedBody:
+            String
+
+    private lateinit var mockExecutor:
+            BackgroundTaskExecutor
+
+    private lateinit var httpCallBodyView:
+            HttpCallBodyView
+
+    private val jsonContentTypeHeader:
+            HttpHeader
+        get() {
+
+            return HttpHeader(
+                name = "Content-Type",
+                values = listOf(
+                    HttpHeaderValue(
+                        "application/json"
+                    )
+                )
+            )
+        }
+
+    private val xmlContentTypeHeader:
+            HttpHeader
+        get() {
+
+            return HttpHeader(
+                name = "Content-Type",
+                values = listOf(
+                    HttpHeaderValue(
+                        "application/xml"
+                    )
+                )
+            )
+        }
+
+    @Before
+    fun setUp() {
+
+        responseBody =
+            "response body"
+
+        requestPayload =
+            "payload"
+
+        error =
+            "error"
+
+        formattedBody =
+            "formatted body"
+
+        httpCallRecord =
+            mockk(relaxed = true)
+
+        repo =
+            mockk(relaxed = true)
+
+        viewModel =
+            mockk(relaxed = true)
+
+        factory =
+            mockk(relaxed = true)
+
+        mockExecutor =
+            mockk(relaxed = true)
+
+        httpCallBodyView =
+            mockk(relaxed = true)
+
+        presenter =
+            HttpCallFragmentPresenter(
+                repo,
+                HTTP_CALL_ID,
+                httpCallBodyView,
+                factory,
+                mockExecutor
+            )
+
+        responseFormatter =
+            mockk(relaxed = true)
+
+        every {
+            httpCallRecord.responseBody
+        } returns responseBody
+
+        every {
+            httpCallRecord.payload
+        } returns requestPayload
+
+        every {
+            httpCallRecord.error
+        } returns error
+
+        every {
+            repo.findById(
+                HTTP_CALL_ID
+            )
+        } returns httpCallRecord
+
+        every {
+            factory.getFor(
+                any()
+            )
+        } returns responseFormatter
+
+        every {
+            responseFormatter.format(
+                any()
+            )
+        } returns formattedBody
     }
 
-  private val xmlContentTypeHeader: HttpHeader
-    get() {
-      val headerValue = HttpHeaderValue("application/xml")
-      val httpHeader = HttpHeader("Content-Type")
-      httpHeader.values = listOf(headerValue)
-      return httpHeader
+    @Test
+    fun shouldInitializeWithJsonFormatterForResponseMode() {
+
+        every {
+            httpCallRecord.getResponseHeader(
+                "Content-Type"
+            )
+        } returns jsonContentTypeHeader
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            RESPONSE_MODE
+        )
+
+        verify(exactly = 1) {
+            factory.getFor(
+                "application/json"
+            )
+        }
+
+        verify(exactly = 1) {
+            responseFormatter.format(
+                responseBody
+            )
+        }
+
+        verify(exactly = 1) {
+            viewModel.init(
+                formattedBody
+            )
+        }
     }
 
-  @Before
-  @Throws(Exception::class)
-  fun setUp() {
-    responseBody = "response body"
-    requestPayload = "payload"
-    error = "error"
-    formattedBody = "formatted body"
-    httpCallRecord = mockk(relaxed = true)
-    repo = mockk(relaxed = true)
-    viewModel = mockk(relaxed = true)
-    factory = mockk(relaxed = true)
-    mockExecutor = mockk(relaxed = true)
-    httpCallBodyView = mockk(relaxed = true)
-    presenter = HttpCallFragmentPresenter(
-      repo,
-      HTTP_CALL_ID,
-      httpCallBodyView,
-      factory,
-      mockExecutor
-    )
-    responseFormatter = mockk()
-    every { httpCallRecord.responseBody } returns responseBody
-    every { httpCallRecord.responseBody }.returns(responseBody)
-    every { httpCallRecord.payload }.returns(requestPayload)
-    every { httpCallRecord.error }.returns(error)
-    every { repo.findById(HTTP_CALL_ID) }.returns(httpCallRecord)
-    every { factory.getFor(any()) }.returns(responseFormatter)
-    every { responseFormatter.format(any()) }.returns(formattedBody)
-  }
+    @Test
+    fun shouldInitializeWithXmlFormatterForResponseMode() {
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldInitializeWithJsonFormatterForResponseMode() {
-    val httpHeader = jsonContentTypeHeader
-    every { httpCallRecord.getResponseHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, RESPONSE_MODE)
+        every {
+            httpCallRecord.getResponseHeader(
+                "Content-Type"
+            )
+        } returns xmlContentTypeHeader
 
-    verify { factory.getFor("application/json") }
-    verify { responseFormatter.format(responseBody) }
-    verify { viewModel.init(formattedBody) }
-  }
+        resolveBackgroundTask()
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldInitializeWithXmlFormatterForResponseMode() {
-    val httpHeader = xmlContentTypeHeader
-    every { httpCallRecord.getResponseHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, RESPONSE_MODE)
+        presenter.init(
+            viewModel,
+            RESPONSE_MODE
+        )
 
-    verify { factory.getFor("application/xml") }
-    verify { responseFormatter.format(responseBody) }
-    verify { viewModel.init(formattedBody) }
-  }
+        verify(exactly = 1) {
+            factory.getFor(
+                "application/xml"
+            )
+        }
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldInitializeWithErrorContent() {
-    every { httpCallRecord.getResponseHeader("Content-Type") } returns null
-    resolveBackgroundTask()
+        verify(exactly = 1) {
+            responseFormatter.format(
+                responseBody
+            )
+        }
 
-    presenter.init(viewModel, ERROR_MODE)
-
-    verify { viewModel.init(error) }
-  }
-
-  @Test
-  @Throws(Exception::class)
-  fun shouldInitializeWithJsonFormatterForRequestMode() {
-    val httpHeader = jsonContentTypeHeader
-    every { httpCallRecord.getRequestHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, REQUEST_MODE)
-
-    verify { factory.getFor("application/json") }
-    verify { responseFormatter.format(requestPayload) }
-    verify { viewModel.init(formattedBody) }
-  }
-
-  @Test
-  @Throws(Exception::class)
-  fun shouldInitializeWithXmlFormatterForRequestMode() {
-    val httpHeader = xmlContentTypeHeader
-    every { httpCallRecord.getRequestHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, REQUEST_MODE)
-
-    verify { factory.getFor("application/xml") }
-    verify { responseFormatter.format(requestPayload) }
-    verify { viewModel.init(formattedBody) }
-  }
-
-  @Test
-  @Throws(Exception::class)
-  fun shouldUsePlainTextFormatterWhenContentTypeHeaderNotFound() {
-    every { httpCallRecord.getRequestHeader("Content-Type") } returns (null)
-    resolveBackgroundTask()
-    presenter.init(viewModel, REQUEST_MODE)
-
-    verify { factory wasNot Called }
-    verify { viewModel.init(requestPayload) }
-  }
-
-  @Test
-  @Throws(Exception::class)
-  fun shouldNotifyViewOnFormattingDone() {
-    val httpHeader = xmlContentTypeHeader
-    every { httpCallRecord.getRequestHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, REQUEST_MODE)
-
-    verify { viewModel.init(any()) }
-    verify { httpCallBodyView.onFormattingDone() }
-  }
-
-  @Test
-  @Throws(Exception::class)
-  fun shouldReturnBoundsToHighlight() {
-    every { responseFormatter.format(any()) } returns "ABC0124abc"
-    val httpHeader = jsonContentTypeHeader
-    every { httpCallRecord.getRequestHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, REQUEST_MODE)
-
-    presenter.searchInBody("abc")
-
-    verify { httpCallBodyView.removeOldHighlightedSpans() }
-    verify {
-      httpCallBodyView.highlightBounds(match { item ->
-        val firstBound = item[0]
-        assertThat(firstBound.left, `is`(0))
-        assertThat(firstBound.right, `is`(3))
-        val secondBound = item[1]
-        assertThat(secondBound.left, `is`(7))
-        assertThat(secondBound.right, `is`(10))
-        true
-      })
+        verify(exactly = 1) {
+            viewModel.init(
+                formattedBody
+            )
+        }
     }
-  }
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldNotHighlightSpansWhenPatternIsEmpty() {
-    every { responseFormatter.format(any()) } returns "ABC0124abc"
-    val httpHeader = jsonContentTypeHeader
-    every { httpCallRecord.getRequestHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, REQUEST_MODE)
+    @Test
+    fun shouldInitializeWithErrorContent() {
 
-    presenter.searchInBody("")
+        every {
+            httpCallRecord.getResponseHeader(
+                "Content-Type"
+            )
+        } returns null
 
-    verify { httpCallBodyView.removeOldHighlightedSpans() }
-    verify(exactly = 0) { httpCallBodyView.highlightBounds(any()) }
-  }
+        resolveBackgroundTask()
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldNotHighlightSpansWhenPatternNotFound() {
-    every { responseFormatter.format(any()) } returns "ABC0124abc"
-    val httpHeader = jsonContentTypeHeader
-    every { httpCallRecord.getRequestHeader("Content-Type") } returns httpHeader
-    resolveBackgroundTask()
-    presenter.init(viewModel, REQUEST_MODE)
+        presenter.init(
+            viewModel,
+            ERROR_MODE
+        )
 
-    presenter.searchInBody("789")
-
-    verify { httpCallBodyView.removeOldHighlightedSpans() }
-    verify(exactly = 0) { httpCallBodyView.highlightBounds(any()) }
-  }
-
-  private fun resolveBackgroundTask() {
-    every { mockExecutor.execute(any<BackgroundTask<String>>()) } answers {
-      val backgroundTask = this.firstArg<BackgroundTask<String>>()
-      backgroundTask.onResult(backgroundTask.onExecute())
+        verify(exactly = 1) {
+            viewModel.init(
+                error
+            )
+        }
     }
-  }
 
-  companion object {
-    const val HTTP_CALL_ID: Long = 5
-  }
+    @Test
+    fun shouldInitializeWithJsonFormatterForRequestMode() {
+
+        every {
+            httpCallRecord.getRequestHeader(
+                "Content-Type"
+            )
+        } returns jsonContentTypeHeader
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            REQUEST_MODE
+        )
+
+        verify(exactly = 1) {
+            factory.getFor(
+                "application/json"
+            )
+        }
+
+        verify(exactly = 1) {
+            responseFormatter.format(
+                requestPayload
+            )
+        }
+
+        verify(exactly = 1) {
+            viewModel.init(
+                formattedBody
+            )
+        }
+    }
+
+    @Test
+    fun shouldInitializeWithXmlFormatterForRequestMode() {
+
+        every {
+            httpCallRecord.getRequestHeader(
+                "Content-Type"
+            )
+        } returns xmlContentTypeHeader
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            REQUEST_MODE
+        )
+
+        verify(exactly = 1) {
+            factory.getFor(
+                "application/xml"
+            )
+        }
+
+        verify(exactly = 1) {
+            responseFormatter.format(
+                requestPayload
+            )
+        }
+
+        verify(exactly = 1) {
+            viewModel.init(
+                formattedBody
+            )
+        }
+    }
+
+    @Test
+    fun shouldUsePlainTextFormatterWhenContentTypeHeaderNotFound() {
+
+        every {
+            httpCallRecord.getRequestHeader(
+                "Content-Type"
+            )
+        } returns null
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            REQUEST_MODE
+        )
+
+        verify {
+            factory wasNot Called
+        }
+
+        verify(exactly = 1) {
+            viewModel.init(
+                requestPayload
+            )
+        }
+    }
+
+    @Test
+    fun shouldNotifyViewOnFormattingDone() {
+
+        every {
+            httpCallRecord.getRequestHeader(
+                "Content-Type"
+            )
+        } returns xmlContentTypeHeader
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            REQUEST_MODE
+        )
+
+        verify(exactly = 1) {
+            viewModel.init(
+                any()
+            )
+        }
+
+        verify(exactly = 1) {
+            httpCallBodyView
+                .onFormattingDone()
+        }
+    }
+
+    @Test
+    fun shouldReturnBoundsToHighlight() {
+
+        every {
+            responseFormatter.format(
+                any()
+            )
+        } returns "ABC0124abc"
+
+        every {
+            httpCallRecord.getRequestHeader(
+                "Content-Type"
+            )
+        } returns jsonContentTypeHeader
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            REQUEST_MODE
+        )
+
+        presenter.searchInBody(
+            "abc"
+        )
+
+        verify(exactly = 1) {
+            httpCallBodyView
+                .removeOldHighlightedSpans()
+        }
+
+        verify {
+            httpCallBodyView.highlightBounds(
+                match { item ->
+
+                    val firstBound =
+                        item[0]
+
+                    assertThat(
+                        firstBound.left,
+                        `is`(0)
+                    )
+
+                    assertThat(
+                        firstBound.right,
+                        `is`(3)
+                    )
+
+                    val secondBound =
+                        item[1]
+
+                    assertThat(
+                        secondBound.left,
+                        `is`(7)
+                    )
+
+                    assertThat(
+                        secondBound.right,
+                        `is`(10)
+                    )
+
+                    true
+                }
+            )
+        }
+    }
+
+    @Test
+    fun shouldNotHighlightSpansWhenPatternIsEmpty() {
+
+        every {
+            responseFormatter.format(
+                any()
+            )
+        } returns "ABC0124abc"
+
+        every {
+            httpCallRecord.getRequestHeader(
+                "Content-Type"
+            )
+        } returns jsonContentTypeHeader
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            REQUEST_MODE
+        )
+
+        presenter.searchInBody(
+            ""
+        )
+
+        verify(exactly = 1) {
+            httpCallBodyView
+                .removeOldHighlightedSpans()
+        }
+
+        verify(exactly = 0) {
+            httpCallBodyView
+                .highlightBounds(any())
+        }
+    }
+
+    @Test
+    fun shouldNotHighlightSpansWhenPatternNotFound() {
+
+        every {
+            responseFormatter.format(
+                any()
+            )
+        } returns "ABC0124abc"
+
+        every {
+            httpCallRecord.getRequestHeader(
+                "Content-Type"
+            )
+        } returns jsonContentTypeHeader
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            REQUEST_MODE
+        )
+
+        presenter.searchInBody(
+            "789"
+        )
+
+        verify(exactly = 1) {
+            httpCallBodyView
+                .removeOldHighlightedSpans()
+        }
+
+        verify(exactly = 0) {
+            httpCallBodyView
+                .highlightBounds(any())
+        }
+    }
+
+    @Test
+    fun shouldHandleNullBodyAsEmptyString() {
+
+        every {
+            httpCallRecord.responseBody
+        } returns null
+
+        every {
+            httpCallRecord.getResponseHeader(
+                "Content-Type"
+            )
+        } returns null
+
+        resolveBackgroundTask()
+
+        presenter.init(
+            viewModel,
+            RESPONSE_MODE
+        )
+
+        verify(exactly = 1) {
+            viewModel.init("")
+        }
+    }
+
+    private fun resolveBackgroundTask() {
+
+        every {
+            mockExecutor.execute(
+                any<BackgroundTask<String>>()
+            )
+        } answers {
+
+            val backgroundTask =
+                firstArg<
+                    BackgroundTask<String>
+                >()
+
+            backgroundTask.onResult(
+                backgroundTask.onExecute()
+            )
+        }
+    }
+
+    companion object {
+
+        const val HTTP_CALL_ID =
+            5L
+    }
 }
