6d50f1c14ae5a4c15aa5f0588b314c75f1e956ca
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:35:22 +0300

    Update HttpHeaderValueCursorParser.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/database/HttpHeaderValueCursorParser.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/database/HttpHeaderValueCursorParser.kt
@@ -1,19 +1,32 @@
 package com.prateekj.snooper.networksnooper.database
 
 import android.database.Cursor
-
+import android.provider.BaseColumns._ID
 import com.prateekj.snooper.database.CursorParser
+import com.prateekj.snooper.networksnooper.database.HttpCallRecordContract.Companion.COLUMN_HEADER_VALUE
 import com.prateekj.snooper.networksnooper.model.HttpHeaderValue
 
-import android.provider.BaseColumns._ID
-import com.prateekj.snooper.networksnooper.database.HttpCallRecordContract.Companion.COLUMN_HEADER_VALUE
+class HttpHeaderValueCursorParser :
+    CursorParser<HttpHeaderValue> {
 
-class HttpHeaderValueCursorParser : CursorParser<HttpHeaderValue> {
+    override fun parse(
+        cursor: Cursor
+    ): HttpHeaderValue {
 
-  override fun parse(cursor: Cursor): HttpHeaderValue {
-    return HttpHeaderValue().apply {
-      setId(cursor.getInt(cursor.getColumnIndexOrThrow(_ID)))
-      value = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_HEADER_VALUE))
+        return HttpHeaderValue().apply {
+
+            setId(
+                cursor.getInt(
+                    cursor.getColumnIndexOrThrow(_ID)
+                )
+            )
+
+            value =
+                cursor.getString(
+                    cursor.getColumnIndexOrThrow(
+                        COLUMN_HEADER_VALUE
+                    )
+                )
+        }
     }
-  }
 }
