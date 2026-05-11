ba0efbd1b2670285d70df02cee01cd99a90941dd
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 00:51:20 +0300

    Update ViewMatcherIdlingResource.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/espresso/ViewMatcherIdlingResource.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/espresso/ViewMatcherIdlingResource.kt
@@ -1,69 +1,114 @@
 package com.prateekj.snooper.espresso
 
-import androidx.test.espresso.IdlingResource
 import android.view.View
-
+import androidx.test.espresso.IdlingResource
 import com.prateekj.snooper.utils.Logger
-
 import org.hamcrest.Matcher
 
-import java.lang.Thread.sleep
-
 class ViewMatcherIdlingResource(
-  private val waitTimeInMillis: Int,
-  private val viewMatcher: Matcher<in View>,
-  private val view: View
+    private val waitTimeInMillis: Int,
+    private val viewMatcher: Matcher<in View>,
+    private val view: View
 ) : IdlingResource {
-  private var resourceCallback: IdlingResource.ResourceCallback? = null
-  private var startTime: Long = 0
-  var isMatched: Boolean = false
-    private set
 
-  init {
-    isMatched = false
-  }
+    private var resourceCallback:
+            IdlingResource.ResourceCallback? = null
 
-  override fun getName(): String {
-    return this.javaClass.name
-  }
+    private var startTime: Long =
+        0
 
-  override fun isIdleNow(): Boolean {
-    val timeExceeded = now() - startTime >= waitTimeInMillis
-    if (isMatched || timeExceeded) {
-      resourceCallback!!.onTransitionToIdle()
+    var isMatched: Boolean =
+        false
+        private set
+
+    init {
+
+        isMatched = false
     }
-    return isMatched || timeExceeded
-  }
 
-  override fun registerIdleTransitionCallback(resourceCallback: IdlingResource.ResourceCallback) {
-    startTime = now()
-    this.resourceCallback = resourceCallback
-    pollViewUntilMatchesOrTimeout(resourceCallback)
-  }
+    override fun getName(): String {
 
-  private fun pollViewUntilMatchesOrTimeout(resourceCallback: IdlingResource.ResourceCallback) {
-    val thread = Thread(Runnable {
-      while (!viewMatcher.matches(view) && now() - startTime < waitTimeInMillis) {
-        try {
-          Logger.d(TAG, "polling view to match $viewMatcher")
-          sleep(MATCHER_POLLING_INTERVAL_IN_MILLIS.toLong())
-        } catch (ignored: InterruptedException) {
+        return this.javaClass.name
+    }
+
+    override fun isIdleNow(): Boolean {
+
+        val timeExceeded =
+            now() - startTime >= waitTimeInMillis
+
+        if (
+            isMatched ||
+            timeExceeded
+        ) {
+
+            resourceCallback
+                ?.onTransitionToIdle()
         }
 
-      }
-      isMatched = viewMatcher.matches(view)
-      resourceCallback.onTransitionToIdle()
-    })
-    thread.start()
-  }
+        return isMatched || timeExceeded
+    }
 
-  private fun now(): Long {
-    return System.currentTimeMillis()
-  }
+    override fun registerIdleTransitionCallback(
+        resourceCallback: IdlingResource.ResourceCallback
+    ) {
 
-  companion object {
+        startTime = now()
 
-    private const val MATCHER_POLLING_INTERVAL_IN_MILLIS = 100
-    val TAG = ViewMatcherIdlingResource::class.java.simpleName
-  }
+        this.resourceCallback =
+            resourceCallback
+
+        pollViewUntilMatchesOrTimeout(
+            resourceCallback
+        )
+    }
+
+    private fun pollViewUntilMatchesOrTimeout(
+        resourceCallback: IdlingResource.ResourceCallback
+    ) {
+
+        val thread =
+            Thread {
+
+                while (
+                    !viewMatcher.matches(view) &&
+                    now() - startTime < waitTimeInMillis
+                ) {
+
+                    try {
+
+                        Logger.d(
+                            TAG,
+                            "polling view to match $viewMatcher"
+                        )
+
+                        Thread.sleep(
+                            MATCHER_POLLING_INTERVAL_IN_MILLIS.toLong()
+                        )
+
+                    } catch (_: InterruptedException) {
+                    }
+                }
+
+                isMatched =
+                    viewMatcher.matches(view)
+
+                resourceCallback.onTransitionToIdle()
+            }
+
+        thread.start()
+    }
+
+    private fun now(): Long {
+
+        return System.currentTimeMillis()
+    }
+
+    companion object {
+
+        private const val MATCHER_POLLING_INTERVAL_IN_MILLIS =
+            100
+
+        val TAG =
+            ViewMatcherIdlingResource::class.java.simpleName
+    }
 }
