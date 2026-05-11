956af6565ccb529722b0938f0ef37566fa757a93
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:36:33 +0300

    Update SnooperRepo.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/database/SnooperRepo.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/database/SnooperRepo.kt
@@ -1,6 +1,5 @@
 package com.prateekj.snooper.networksnooper.database
 
-
 import android.content.ContentValues
 import android.content.Context
 import android.database.Cursor
@@ -32,186 +31,458 @@ import com.prateekj.snooper.networksnooper.database.HttpCallRecordContract.Compa
 import com.prateekj.snooper.networksnooper.model.HttpCallRecord
 import com.prateekj.snooper.networksnooper.model.HttpHeader
 import com.prateekj.snooper.networksnooper.model.HttpHeaderValue
-import java.util.ArrayList
 
-class SnooperRepo(context: Context) {
+class SnooperRepo(
+    context: Context
+) {
 
-  private val dbWriteHelper: SnooperDbHelper = SnooperDbHelper.create(context)
-  private val dbReadHelper: SnooperDbHelper = SnooperDbHelper.create(context)
+    private val dbWriteHelper =
+        SnooperDbHelper.create(context)
 
-  fun save(httpCallRecord: HttpCallRecord): Long {
-    val database = dbWriteHelper.writableDatabase
-    val values = ContentValues()
-    values.put(COLUMN_URL, httpCallRecord.url)
-    values.put(COLUMN_PAYLOAD, httpCallRecord.payload)
-    values.put(COLUMN_RESPONSE_BODY, httpCallRecord.responseBody)
-    values.put(COLUMN_METHOD, httpCallRecord.method)
-    values.put(COLUMN_STATUSCODE, httpCallRecord.statusCode)
-    values.put(COLUMN_STATUSTEXT, httpCallRecord.statusText)
-    values.put(COLUMN_DATE, httpCallRecord.date!!.time)
-    values.put(COLUMN_ERROR, httpCallRecord.error)
-    val httpCallRecordId = database.insert(HTTP_CALL_RECORD_TABLE_NAME, null, values)
-    try {
-      database.beginTransaction()
-      saveHeaders(database, httpCallRecordId, httpCallRecord.requestHeaders!!, "req")
-      saveHeaders(database, httpCallRecordId, httpCallRecord.responseHeaders!!, "res")
-      database.setTransactionSuccessful()
-    } finally {
-      database.endTransaction()
-      database.close()
+    private val dbReadHelper =
+        SnooperDbHelper.create(context)
+
+    fun save(
+        httpCallRecord: HttpCallRecord
+    ): Long {
+
+        val database =
+            dbWriteHelper.writableDatabase
+
+        return try {
+
+            database.beginTransaction()
+
+            val values = ContentValues().apply {
+
+                put(
+                    COLUMN_URL,
+                    httpCallRecord.url
+                )
+
+                put(
+                    COLUMN_PAYLOAD,
+                    httpCallRecord.payload
+                )
+
+                put(
+                    COLUMN_RESPONSE_BODY,
+                    httpCallRecord.responseBody
+                )
+
+                put(
+                    COLUMN_METHOD,
+                    httpCallRecord.method
+                )
+
+                put(
+                    COLUMN_STATUSCODE,
+                    httpCallRecord.statusCode
+                )
+
+                put(
+                    COLUMN_STATUSTEXT,
+                    httpCallRecord.statusText
+                )
+
+                put(
+                    COLUMN_DATE,
+                    httpCallRecord.date?.time
+                )
+
+                put(
+                    COLUMN_ERROR,
+                    httpCallRecord.error
+                )
+            }
+
+            val httpCallRecordId =
+                database.insert(
+                    HTTP_CALL_RECORD_TABLE_NAME,
+                    null,
+                    values
+                )
+
+            saveHeaders(
+                database,
+                httpCallRecordId,
+                httpCallRecord.requestHeaders
+                    ?: emptyList(),
+                "req"
+            )
+
+            saveHeaders(
+                database,
+                httpCallRecordId,
+                httpCallRecord.responseHeaders
+                    ?: emptyList(),
+                "res"
+            )
+
+            database.setTransactionSuccessful()
+
+            httpCallRecordId
+
+        } finally {
+
+            database.endTransaction()
+            database.close()
+        }
     }
-    return httpCallRecordId
-  }
 
-  fun findAllSortByDate(): List<HttpCallRecord> {
-    val httpCallRecords = ArrayList<HttpCallRecord>()
-    val cursorParser = HttpCallRecordCursorParser()
-    val database = dbReadHelper.readableDatabase
-    val cursor = database.rawQuery(HTTP_CALL_RECORD_GET_SORT_BY_DATE, null)
-    while (cursor.moveToNext()) {
-      httpCallRecords.add(cursorParser.parse(cursor))
+    fun findAllSortByDate():
+            List<HttpCallRecord> {
+
+        val database =
+            dbReadHelper.readableDatabase
+
+        val records =
+            mutableListOf<HttpCallRecord>()
+
+        val parser =
+            HttpCallRecordCursorParser()
+
+        val cursor =
+            database.rawQuery(
+                HTTP_CALL_RECORD_GET_SORT_BY_DATE,
+                null
+            )
+
+        cursor.use {
+
+            while (it.moveToNext()) {
+
+                records.add(
+                    parser.parse(it)
+                )
+            }
+        }
+
+        database.close()
+
+        return records
     }
-    cursor.close()
-    database.close()
-    return httpCallRecords
-  }
 
-  fun searchHttpRecord(text: String): List<HttpCallRecord> {
-    val httpCallRecords = ArrayList<HttpCallRecord>()
-    val cursorParser = HttpCallRecordCursorParser()
-    val database = dbReadHelper.readableDatabase
-    val cursor = database.rawQuery(
-      HTTP_CALL_RECORD_SEARCH,
-      arrayOf(likeParam(text), likeParam(text), likeParam(text), likeParam(text))
-    )
-    while (cursor.moveToNext()) {
-      httpCallRecords.add(cursorParser.parse(cursor))
+    fun searchHttpRecord(
+        text: String
+    ): List<HttpCallRecord> {
+
+        val database =
+            dbReadHelper.readableDatabase
+
+        val records =
+            mutableListOf<HttpCallRecord>()
+
+        val parser =
+            HttpCallRecordCursorParser()
+
+        val likeText =
+            "%$text%"
+
+        val cursor =
+            database.rawQuery(
+                HTTP_CALL_RECORD_SEARCH,
+                arrayOf(
+                    likeText,
+                    likeText,
+                    likeText,
+                    likeText
+                )
+            )
+
+        cursor.use {
+
+            while (it.moveToNext()) {
+
+                records.add(
+                    parser.parse(it)
+                )
+            }
+        }
+
+        database.close()
+
+        return records
     }
-    cursor.close()
-    database.close()
-    return httpCallRecords
-  }
 
-  private fun likeParam(text: String): String {
-    return "%$text%"
-  }
+    fun findAllSortByDateAfter(
+        id: Long,
+        pageSize: Int
+    ): MutableList<HttpCallRecord> {
 
+        val database =
+            dbReadHelper.readableDatabase
 
-  fun findAllSortByDateAfter(id: Long, pageSize: Int): MutableList<HttpCallRecord> {
-    val httpCallRecords = ArrayList<HttpCallRecord>()
-    val cursorParser = HttpCallRecordCursorParser()
-    val database = dbReadHelper.readableDatabase
-    val cursor: Cursor
-    cursor = if (id == -1L) {
-      database.rawQuery(
-        HTTP_CALL_RECORD_GET_SORT_BY_DATE_WITH_SIZE,
-        arrayOf(pageSize.toString())
-      )
-    } else {
-      database.rawQuery(
-        HTTP_CALL_RECORD_GET_NEXT_SORT_BY_DATE_WITH_SIZE,
-        arrayOf(id.toString(), pageSize.toString())
-      )
+        val records =
+            mutableListOf<HttpCallRecord>()
+
+        val parser =
+            HttpCallRecordCursorParser()
+
+        val cursor: Cursor =
+            if (id == -1L) {
+
+                database.rawQuery(
+                    HTTP_CALL_RECORD_GET_SORT_BY_DATE_WITH_SIZE,
+                    arrayOf(
+                        pageSize.toString()
+                    )
+                )
+
+            } else {
+
+                database.rawQuery(
+                    HTTP_CALL_RECORD_GET_NEXT_SORT_BY_DATE_WITH_SIZE,
+                    arrayOf(
+                        id.toString(),
+                        pageSize.toString()
+                    )
+                )
+            }
+
+        cursor.use {
+
+            while (it.moveToNext()) {
+
+                records.add(
+                    parser.parse(it)
+                )
+            }
+        }
+
+        database.close()
+
+        return records
     }
-    while (cursor.moveToNext()) {
-      httpCallRecords.add(cursorParser.parse(cursor))
+
+    fun findById(
+        id: Long
+    ): HttpCallRecord {
+
+        val database =
+            dbReadHelper.readableDatabase
+
+        val parser =
+            HttpCallRecordCursorParser()
+
+        val cursor =
+            database.rawQuery(
+                HTTP_CALL_RECORD_GET_BY_ID,
+                arrayOf(id.toString())
+            )
+
+        lateinit var record: HttpCallRecord
+
+        cursor.use {
+
+            if (it.moveToFirst()) {
+
+                record =
+                    parser.parse(it)
+
+                record.requestHeaders =
+                    findHeader(
+                        database,
+                        record.id,
+                        "req"
+                    )
+
+                record.responseHeaders =
+                    findHeader(
+                        database,
+                        record.id,
+                        "res"
+                    )
+            }
+        }
+
+        database.close()
+
+        return record
     }
-    cursor.close()
-    database.close()
-    return httpCallRecords
-  }
 
+    fun deleteAll() {
 
-  fun findById(id: Long): HttpCallRecord {
-    val database = dbReadHelper.readableDatabase
-    val cursorParser = HttpCallRecordCursorParser()
-    val cursor =
-      database.rawQuery(HTTP_CALL_RECORD_GET_BY_ID, arrayOf(java.lang.Long.toString(id)))
-    cursor.moveToNext()
-    val httpCallRecord = cursorParser.parse(cursor)
-    httpCallRecord.requestHeaders = findHeader(database, httpCallRecord.id, "req")
-    httpCallRecord.responseHeaders = findHeader(database, httpCallRecord.id, "res")
-    database.close()
-    return httpCallRecord
-  }
+        val database =
+            dbWriteHelper.writableDatabase
 
-  fun deleteAll() {
-    val database = dbWriteHelper.writableDatabase
-    try {
-      database.beginTransaction()
-      database.delete(HTTP_CALL_RECORD_TABLE_NAME, null, null)
-      database.setTransactionSuccessful()
-    } finally {
-      database.endTransaction()
-      database.close()
+        try {
+
+            database.beginTransaction()
+
+            database.delete(
+                HTTP_CALL_RECORD_TABLE_NAME,
+                null,
+                null
+            )
+
+            database.setTransactionSuccessful()
+
+        } finally {
+
+            database.endTransaction()
+            database.close()
+        }
     }
-  }
 
-  private fun findHeader(
-    database: SQLiteDatabase,
-    callId: Long,
-    headerType: String
-  ): List<HttpHeader> {
-    val httpHeaders = ArrayList<HttpHeader>()
-    val cursorParser = HttpHeaderCursorParser()
-    val cursor = database.rawQuery(
-      HTTP_HEADER_GET_BY_CALL_ID,
-      arrayOf(callId.toString(), headerType)
-    )
-    while (cursor.moveToNext()) {
-      val httpHeader = cursorParser.parse(cursor)
-      httpHeader.values = findHeaderValue(database, httpHeader.id)
-      httpHeaders.add(httpHeader)
+    private fun findHeader(
+        database: SQLiteDatabase,
+        callId: Long,
+        headerType: String
+    ): List<HttpHeader> {
+
+        val headers =
+            mutableListOf<HttpHeader>()
+
+        val parser =
+            HttpHeaderCursorParser()
+
+        val cursor =
+            database.rawQuery(
+                HTTP_HEADER_GET_BY_CALL_ID,
+                arrayOf(
+                    callId.toString(),
+                    headerType
+                )
+            )
+
+        cursor.use {
+
+            while (it.moveToNext()) {
+
+                val header =
+                    parser.parse(it)
+
+                header.values =
+                    findHeaderValue(
+                        database,
+                        header.id
+                    )
+
+                headers.add(header)
+            }
+        }
+
+        return headers
     }
-    cursor.close()
-    return httpHeaders
-  }
 
-  private fun findHeaderValue(database: SQLiteDatabase, headerId: Int): List<HttpHeaderValue> {
-    val httpHeaderValues = ArrayList<HttpHeaderValue>()
-    val cursorParser = HttpHeaderValueCursorParser()
-    val cursor = database.rawQuery(
-      HTTP_HEADER_VALUE_GET_BY_HEADER_ID,
-      arrayOf(Integer.toString(headerId))
-    )
-    while (cursor.moveToNext()) {
-      httpHeaderValues.add(cursorParser.parse(cursor))
+    private fun findHeaderValue(
+        database: SQLiteDatabase,
+        headerId: Int
+    ): List<HttpHeaderValue> {
+
+        val values =
+            mutableListOf<HttpHeaderValue>()
+
+        val parser =
+            HttpHeaderValueCursorParser()
+
+        val cursor =
+            database.rawQuery(
+                HTTP_HEADER_VALUE_GET_BY_HEADER_ID,
+                arrayOf(
+                    headerId.toString()
+                )
+            )
+
+        cursor.use {
+
+            while (it.moveToNext()) {
+
+                values.add(
+                    parser.parse(it)
+                )
+            }
+        }
+
+        return values
     }
-    cursor.close()
-    return httpHeaderValues
-  }
 
-  private fun saveHeaders(
-    database: SQLiteDatabase,
-    httpCallRecordId: Long,
-    requestHeaders: List<HttpHeader>,
-    headerType: String
-  ) {
-    for (httpHeader in requestHeaders) {
-      saveHeader(database, httpHeader, httpCallRecordId, headerType)
+    private fun saveHeaders(
+        database: SQLiteDatabase,
+        httpCallRecordId: Long,
+        headers: List<HttpHeader>,
+        headerType: String
+    ) {
+
+        headers.forEach { header ->
+
+            saveHeader(
+                database,
+                header,
+                httpCallRecordId,
+                headerType
+            )
+        }
     }
-  }
 
-  private fun saveHeader(
-    database: SQLiteDatabase,
-    httpHeader: HttpHeader,
-    httpCallRecordId: Long,
-    headerType: String
-  ) {
-    val values = ContentValues()
-    values.put(COLUMN_HEADER_NAME, httpHeader.name)
-    values.put(COLUMN_HEADER_TYPE, headerType)
-    values.put(COLUMN_HTTP_CALL_RECORD_ID, httpCallRecordId)
-    val headerId = database.insert(HEADER_TABLE_NAME, null, values)
-    for (httpHeaderValue in httpHeader.values) {
-      saveHeaderValue(database, httpHeaderValue, headerId)
+    private fun saveHeader(
+        database: SQLiteDatabase,
+        httpHeader: HttpHeader,
+        httpCallRecordId: Long,
+        headerType: String
+    ) {
+
+        val values =
+            ContentValues().apply {
+
+                put(
+                    COLUMN_HEADER_NAME,
+                    httpHeader.name
+                )
+
+                put(
+                    COLUMN_HEADER_TYPE,
+                    headerType
+                )
+
+                put(
+                    COLUMN_HTTP_CALL_RECORD_ID,
+                    httpCallRecordId
+                )
+            }
+
+        val headerId =
+            database.insert(
+                HEADER_TABLE_NAME,
+                null,
+                values
+            )
+
+        httpHeader.values.forEach {
+
+            saveHeaderValue(
+                database,
+                it,
+                headerId
+            )
+        }
     }
-  }
 
-  private fun saveHeaderValue(database: SQLiteDatabase, value: HttpHeaderValue, headerId: Long) {
-    val values = ContentValues()
-    values.put(COLUMN_HEADER_VALUE, value.value)
-    values.put(COLUMN_HEADER_ID, headerId)
-    database.insert(HEADER_VALUE_TABLE_NAME, null, values)
-  }
+    private fun saveHeaderValue(
+        database: SQLiteDatabase,
+        value: HttpHeaderValue,
+        headerId: Long
+    ) {
+
+        val values =
+            ContentValues().apply {
+
+                put(
+                    COLUMN_HEADER_VALUE,
+                    value.value
+                )
+
+                put(
+                    COLUMN_HEADER_ID,
+                    headerId
+                )
+            }
+
+        database.insert(
+            HEADER_VALUE_TABLE_NAME,
+            null,
+            values
+        )
+    }
 }
