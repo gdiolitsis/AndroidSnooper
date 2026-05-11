547a3ee11b5918adac74f9566fe1f3485c36a7e9
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:04:48 +0300

    Update RunUsingLooper.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/rules/RunUsingLooper.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/rules/RunUsingLooper.kt
@@ -1,25 +1,42 @@
 package com.prateekj.snooper.rules
 
 import android.os.Looper
-
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
 
 class RunUsingLooper : TestRule {
-  override fun apply(base: Statement, description: Description): Statement {
-    return object : Statement() {
-      @Throws(Throwable::class)
-      override fun evaluate() {
-        try {
-          if (Looper.myLooper() == null) {
-            Looper.prepare()
-          }
-          base.evaluate()
-        } finally {
-          Looper.myLooper()!!.quit()
+
+    override fun apply(
+        base: Statement,
+        description: Description
+    ): Statement {
+
+        return object : Statement() {
+
+            @Throws(Throwable::class)
+            override fun evaluate() {
+
+                var looperPrepared = false
+
+                try {
+
+                    if (Looper.myLooper() == null) {
+
+                        Looper.prepare()
+                        looperPrepared = true
+                    }
+
+                    base.evaluate()
+
+                } finally {
+
+                    if (looperPrepared) {
+
+                        Looper.myLooper()?.quitSafely()
+                    }
+                }
+            }
         }
-      }
     }
-  }
 }
