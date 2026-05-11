3b24f0f386b58028becb35d6f41fb557ac822e8b
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 11:41:07 +0300

    Update SpringHttpRequestTransformerTest.kt

--- Snooper-Spring/src/test/java/org/springframework/http/client/SpringHttpRequestTransformerTest.kt
+++ Snooper-Spring/src/test/java/org/springframework/http/client/SpringHttpRequestTransformerTest.kt
@@ -12,84 +12,197 @@ import org.springframework.http.HttpMethod.POST
 import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus.OK
 import java.io.ByteArrayInputStream
-import java.io.UnsupportedEncodingException
 import java.net.URI.create
 import java.net.UnknownHostException
-import java.util.Arrays
+import java.nio.charset.StandardCharsets
 
 class SpringHttpRequestTransformerTest {
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldTransformHttpCallFromSpringHttpRequest() {
-    val url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0"
-    val uri = create(url)
-    val responseBody = "responseBody"
-    val requestBody = "requestBody"
-    val httpHeaders = HttpHeaders()
-    httpHeaders["Content-Type"] = listOf("application/json")
+    @Test
+    @Throws(Exception::class)
+    fun shouldTransformHttpCallFromSpringHttpRequest() {
 
-    val httpResponse = mockk<ClientHttpResponse>(relaxed = true)
-    every { httpResponse.body } returns ByteArrayInputStream(toBytes(responseBody))
-    every { httpResponse.statusCode } returns OK
-    every { httpResponse.rawStatusCode } returns 200
-    every { httpResponse.headers } returns httpHeaders
-    val httpRequest = mockk<HttpRequest>()
-    every { httpRequest.method } returns POST
-    every { httpRequest.uri } returns uri
-    every { httpRequest.headers } returns httpHeaders
+        val url =
+            "https://ajax.googleapis.com/ajax/services/search/web?v=1.0"
 
-    val transformer = SpringHttpRequestTransformer()
+        val uri = create(url)
 
-    val httpCall = transformer.transform(httpRequest, toBytes(requestBody), httpResponse)
+        val responseBody = "responseBody"
 
-    assertThat<String>(httpCall.method, `is`("POST"))
-    assertThat<String>(httpCall.payload, `is`(requestBody))
-    assertThat<String>(httpCall.url, `is`(url))
-    assertThat<String>(httpCall.responseBody, `is`(responseBody))
-    assertThat<String>(httpCall.statusText, `is`("OK"))
-    assertThat(httpCall.statusCode, `is`(200))
-    assertThat(httpCall.requestHeaders.size, `is`(1))
-    assertThat(httpCall.responseHeaders.size, `is`(1))
-    assertNotNull(httpCall.responseHeaders[CONTENT_TYPE])
-    assertNotNull(httpCall.requestHeaders[CONTENT_TYPE])
-  }
+        val requestBody = "requestBody"
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldTransformHttpCallFromClientSideError() {
-    val url = "https://ajax.googleapis.com/ajax/services/search/web?v=1.0"
-    val uri = create(url)
-    val requestBody = "requestBody"
-    val httpHeaders = HttpHeaders()
-    httpHeaders["Content-Type"] = Arrays.asList("application/json")
+        val httpHeaders = HttpHeaders()
 
-    val httpRequest = mockk<HttpRequest>()
-    every { httpRequest.method } returns POST
-    every { httpRequest.uri } returns uri
-    every { httpRequest.headers } returns httpHeaders
+        httpHeaders["Content-Type"] =
+            listOf("application/json")
 
-    val ioException = UnknownHostException("Unable to connect")
+        val httpResponse =
+            mockk<ClientHttpResponse>(relaxed = true)
 
-    val transformer = SpringHttpRequestTransformer()
+        every {
+            httpResponse.body
+        } returns ByteArrayInputStream(
+            toBytes(responseBody)
+        )
 
-    val httpCall = transformer.transform(httpRequest, toBytes(requestBody), ioException)
+        every {
+            httpResponse.statusCode
+        } returns OK
 
-    assertThat<String>(httpCall.method, `is`("POST"))
-    assertThat<String>(httpCall.payload, `is`(requestBody))
-    assertThat<String>(httpCall.url, `is`(url))
-    assertThat(httpCall.requestHeaders.size, `is`(1))
-    assertNotNull(httpCall.requestHeaders[CONTENT_TYPE])
-    assertThat<String>(httpCall.error, `is`("java.net.UnknownHostException: Unable to connect"))
-  }
+        every {
+            httpResponse.rawStatusCode
+        } returns 200
 
-  fun toBytes(string: String): ByteArray {
-    try {
-      return string.toByteArray(charset("UTF-8"))
-    } catch (e: UnsupportedEncodingException) {
-      e.printStackTrace()
+        every {
+            httpResponse.headers
+        } returns httpHeaders
+
+        val httpRequest =
+            mockk<HttpRequest>()
+
+        every {
+            httpRequest.method
+        } returns POST
+
+        every {
+            httpRequest.uri
+        } returns uri
+
+        every {
+            httpRequest.headers
+        } returns httpHeaders
+
+        val transformer =
+            SpringHttpRequestTransformer()
+
+        val httpCall =
+            transformer.transform(
+                httpRequest,
+                toBytes(requestBody),
+                httpResponse
+            )
+
+        assertThat(httpCall.method, `is`("POST"))
+
+        assertThat(httpCall.payload, `is`(requestBody))
+
+        assertThat(httpCall.url, `is`(url))
+
+        assertThat(
+            httpCall.responseBody,
+            `is`(responseBody)
+        )
+
+        assertThat(
+            httpCall.statusText,
+            `is`("OK")
+        )
+
+        assertThat(
+            httpCall.statusCode,
+            `is`(200)
+        )
+
+        assertThat(
+            httpCall.requestHeaders.size,
+            `is`(1)
+        )
+
+        assertThat(
+            httpCall.responseHeaders.size,
+            `is`(1)
+        )
+
+        assertNotNull(
+            httpCall.responseHeaders[CONTENT_TYPE]
+        )
+
+        assertNotNull(
+            httpCall.requestHeaders[CONTENT_TYPE]
+        )
     }
 
-    return ByteArray(0)
-  }
+    @Test
+    @Throws(Exception::class)
+    fun shouldTransformHttpCallFromClientSideError() {
+
+        val url =
+            "https://ajax.googleapis.com/ajax/services/search/web?v=1.0"
+
+        val uri = create(url)
+
+        val requestBody = "requestBody"
+
+        val httpHeaders = HttpHeaders()
+
+        httpHeaders["Content-Type"] =
+            listOf("application/json")
+
+        val httpRequest =
+            mockk<HttpRequest>()
+
+        every {
+            httpRequest.method
+        } returns POST
+
+        every {
+            httpRequest.uri
+        } returns uri
+
+        every {
+            httpRequest.headers
+        } returns httpHeaders
+
+        val ioException =
+            UnknownHostException(
+                "Unable to connect"
+            )
+
+        val transformer =
+            SpringHttpRequestTransformer()
+
+        val httpCall =
+            transformer.transform(
+                httpRequest,
+                toBytes(requestBody),
+                ioException
+            )
+
+        assertThat(httpCall.method, `is`("POST"))
+
+        assertThat(
+            httpCall.payload,
+            `is`(requestBody)
+        )
+
+        assertThat(
+            httpCall.url,
+            `is`(url)
+        )
+
+        assertThat(
+            httpCall.requestHeaders.size,
+            `is`(1)
+        )
+
+        assertNotNull(
+            httpCall.requestHeaders[CONTENT_TYPE]
+        )
+
+        assertThat(
+            httpCall.error,
+            `is`(
+                "java.net.UnknownHostException: Unable to connect"
+            )
+        )
+    }
+
+    private fun toBytes(
+        string: String
+    ): ByteArray {
+
+        return string.toByteArray(
+            StandardCharsets.UTF_8
+        )
+    }
 }
