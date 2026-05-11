7c754fea67a7d3db1d6f2a1915bc5f988b2525dc
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:09:18 +0300

    Update TestUtilities.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/utils/TestUtilities.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/utils/TestUtilities.kt
@@ -1,42 +1,114 @@
 package com.prateekj.snooper.utils
 
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.platform.app.InstrumentationRegistry
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
 import java.util.Calendar
 import java.util.Date
+import java.util.Locale
 
 object TestUtilities {
 
-  @Throws(IOException::class)
-  fun readFileAsStream(assetFileName: String): InputStream {
-    return getInstrumentation().context.assets.open(assetFileName)
-  }
+    @Throws(IOException::class)
+    fun readFileAsStream(
+        assetFileName: String
+    ): InputStream {
 
-  @Throws(IOException::class)
-  fun readFrom(assetFileName: String): String {
-    val inputStream = readFileAsStream(assetFileName)
-    val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-    return bufferedReader.use { it.readText() }
-  }
+        return InstrumentationRegistry
+            .getInstrumentation()
+            .context
+            .assets
+            .open(assetFileName)
+    }
 
-  fun getDate(year: Int, month: Int, day: Int): Date {
-    val instance = Calendar.getInstance()
-    instance.set(year, month, day)
-    return instance.time
-  }
+    @Throws(IOException::class)
+    fun readFrom(
+        assetFileName: String
+    ): String {
 
-  fun getDate(year: Int, month: Int, day: Int, hour: Int, minute: Int, seconds: Int): Date {
-    val instance = Calendar.getInstance()
-    instance.set(year, month, day, hour, minute, seconds)
-    return instance.time
-  }
+        val inputStream =
+            readFileAsStream(assetFileName)
 
-  fun getCalendar(date: Date): Calendar {
-    val instance = Calendar.getInstance()
-    instance.time = date
-    return instance
-  }
+        val bufferedReader =
+            BufferedReader(
+                InputStreamReader(inputStream)
+            )
+
+        return bufferedReader.use {
+            it.readText()
+        }
+    }
+
+    fun getDate(
+        year: Int,
+        month: Int,
+        day: Int
+    ): Date {
+
+        val instance =
+            Calendar.getInstance(
+                Locale.US
+            )
+
+        instance.set(
+            year,
+            month,
+            day,
+            0,
+            0,
+            0
+        )
+
+        instance.set(
+            Calendar.MILLISECOND,
+            0
+        )
+
+        return instance.time
+    }
+
+    fun getDate(
+        year: Int,
+        month: Int,
+        day: Int,
+        hour: Int,
+        minute: Int,
+        seconds: Int
+    ): Date {
+
+        val instance =
+            Calendar.getInstance(
+                Locale.US
+            )
+
+        instance.set(
+            year,
+            month,
+            day,
+            hour,
+            minute,
+            seconds
+        )
+
+        instance.set(
+            Calendar.MILLISECOND,
+            0
+        )
+
+        return instance.time
+    }
+
+    fun getCalendar(
+        date: Date
+    ): Calendar {
+
+        return Calendar.getInstance(
+            Locale.US
+        ).apply {
+
+            time = date
+        }
+    }
 }
