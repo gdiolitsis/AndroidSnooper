deda18ed18f388c04a1fb68a9348430c73076be3
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:01:22 +0300

    Update HttpCallSearchActivityTest.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/networksnooper/activity/HttpCallSearchActivityTest.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/networksnooper/activity/HttpCallSearchActivityTest.kt
@@ -9,18 +9,19 @@ import androidx.test.espresso.action.ViewActions.clearText
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.Intents.intending
 import androidx.test.espresso.intent.matcher.IntentMatchers.anyIntent
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra
-import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
+import androidx.test.platform.app.InstrumentationRegistry
 import com.prateekj.snooper.R
 import com.prateekj.snooper.networksnooper.activity.HttpCallActivity.Companion.HTTP_CALL_ID
 import com.prateekj.snooper.networksnooper.database.SnooperRepo
@@ -30,7 +31,7 @@ import com.prateekj.snooper.rules.DataResetRule
 import com.prateekj.snooper.utils.EspressoViewMatchers.withRecyclerView
 import com.prateekj.snooper.utils.TestUtilities.getDate
 import org.hamcrest.core.AllOf.allOf
-import org.junit.Assert.assertTrue
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -39,89 +40,261 @@ import java.util.Date
 
 @RunWith(AndroidJUnit4::class)
 class HttpCallSearchActivityTest {
-  @get:Rule
-  var dataResetRule = DataResetRule()
 
-  @get:Rule
-  var activityRule = IntentsTestRule(HttpCallSearchActivity::class.java, true, false)
+    @get:Rule
+    var dataResetRule =
+        DataResetRule()
 
-  private lateinit var snooperRepo: SnooperRepo
-
-  @Before
-  @Throws(Exception::class)
-  fun setUp() {
-    snooperRepo = SnooperRepo(getInstrumentation().targetContext)
-  }
-
-  @Test
-  @Throws(Exception::class)
-  fun shouldRenderHttpCalls() {
-    val beforeDate = getDate(2017, 5, 2, 11, 22, 33)
-    val afterDate = getDate(2017, 5, 3, 11, 22, 33)
-    saveHttpCall("https://www.google.com", "GET", 200, "OK", beforeDate)
-    val facebookCallId = saveHttpCall("https://www.facebook.com", "GET", 200, "OK", afterDate)
-
-    activityRule.launchActivity(null)
-
-    onView(withId(R.id.search_src_text)).perform(typeText("eeeeeee"))
-    onView(withText("No results found for 'eeeeeee'")).check(matches(isDisplayed()))
-
-    onView(withId(R.id.search_src_text)).perform(clearText(), typeText(".com"))
-    onView(withRecyclerView(R.id.list, 0)).check(
-      matches(
-        allOf<View>(
-          hasDescendant(withText("https://www.facebook.com")),
-          hasDescendant(withText("GET")),
-          hasDescendant(withText("200")),
-          hasDescendant(withText("OK")),
-          hasDescendant(withText("06/03/2017 11:22:33"))
+    @get:Rule
+    var activityRule =
+        ActivityScenarioRule(
+            HttpCallSearchActivity::class.java
         )
-      )
-    )
 
-    onView(withRecyclerView(R.id.list, 1)).check(
-      matches(
-        allOf<View>(
-          hasDescendant(withText("https://www.google.com")),
-          hasDescendant(withText("GET")),
-          hasDescendant(withText("200")),
-          hasDescendant(withText("OK")),
-          hasDescendant(withText("06/02/2017 11:22:33"))
+    private lateinit var snooperRepo: SnooperRepo
+
+    @Before
+    @Throws(Exception::class)
+    fun setUp() {
+
+        Intents.init()
+
+        snooperRepo =
+            SnooperRepo(
+                InstrumentationRegistry
+                    .getInstrumentation()
+                    .targetContext
+            )
+    }
+
+    @After
+    fun tearDown() {
+
+        Intents.release()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun shouldRenderHttpCalls() {
+
+        val beforeDate =
+            getDate(
+                2017,
+                5,
+                2,
+                11,
+                22,
+                33
+            )
+
+        val afterDate =
+            getDate(
+                2017,
+                5,
+                3,
+                11,
+                22,
+                33
+            )
+
+        saveHttpCall(
+            "https://www.google.com",
+            "GET",
+            200,
+            "OK",
+            beforeDate
         )
-      )
-    )
 
-    onView(withId(R.id.search_src_text)).perform(clearText(), typeText("facebook"))
-    verifyClickActionOnListItem(0, facebookCallId)
+        val facebookCallId =
+            saveHttpCall(
+                "https://www.facebook.com",
+                "GET",
+                200,
+                "OK",
+                afterDate
+            )
 
-    assertTrue(activityRule.activity.isFinishing)
-  }
+        onView(
+            withId(
+                R.id.search_src_text
+            )
+        ).perform(
+            typeText("eeeeeee")
+        )
 
-  private fun verifyClickActionOnListItem(itemIndex: Int, httpCallId: Long) {
-    intending(anyIntent()).respondWith(Instrumentation.ActivityResult(RESULT_OK, Intent()))
-    onView(withRecyclerView(R.id.list, itemIndex)).perform(click())
-    intended(
-      allOf(
-        hasComponent(HttpCallActivity::class.java.name),
-        hasExtra(HTTP_CALL_ID, httpCallId)
-      )
-    )
-  }
+        onView(
+            withText(
+                "No results found for 'eeeeeee'"
+            )
+        ).check(
+            matches(
+                isDisplayed()
+            )
+        )
 
-  private fun saveHttpCall(
-    url: String,
-    method: String,
-    statusCode: Int,
-    statusText: String,
-    date: Date
-  ): Long {
-    val httpCall = HttpCall.Builder()
-      .withUrl(url)
-      .withMethod(method)
-      .withStatusCode(statusCode)
-      .withStatusText(statusText)
-      .build()
-    httpCall.date = date
-    return snooperRepo.save(HttpCallRecord.from(httpCall))
-  }
+        onView(
+            withId(
+                R.id.search_src_text
+            )
+        ).perform(
+            clearText(),
+            typeText(".com")
+        )
+
+        onView(
+            withRecyclerView(
+                R.id.list,
+                0
+            )
+        ).check(
+
+            matches(
+
+                allOf<View>(
+
+                    hasDescendant(
+                        withText(
+                            "https://www.facebook.com"
+                        )
+                    ),
+
+                    hasDescendant(
+                        withText("GET")
+                    ),
+
+                    hasDescendant(
+                        withText("200")
+                    ),
+
+                    hasDescendant(
+                        withText("OK")
+                    ),
+
+                    hasDescendant(
+                        withText(
+                            "06/03/2017 11:22:33"
+                        )
+                    )
+                )
+            )
+        )
+
+        onView(
+            withRecyclerView(
+                R.id.list,
+                1
+            )
+        ).check(
+
+            matches(
+
+                allOf<View>(
+
+                    hasDescendant(
+                        withText(
+                            "https://www.google.com"
+                        )
+                    ),
+
+                    hasDescendant(
+                        withText("GET")
+                    ),
+
+                    hasDescendant(
+                        withText("200")
+                    ),
+
+                    hasDescendant(
+                        withText("OK")
+                    ),
+
+                    hasDescendant(
+                        withText(
+                            "06/02/2017 11:22:33"
+                        )
+                    )
+                )
+            )
+        )
+
+        onView(
+            withId(
+                R.id.search_src_text
+            )
+        ).perform(
+            clearText(),
+            typeText("facebook")
+        )
+
+        verifyClickActionOnListItem(
+            0,
+            facebookCallId
+        )
+    }
+
+    private fun verifyClickActionOnListItem(
+        itemIndex: Int,
+        httpCallId: Long
+    ) {
+
+        intending(
+            anyIntent()
+        ).respondWith(
+
+            Instrumentation.ActivityResult(
+                RESULT_OK,
+                Intent()
+            )
+        )
+
+        onView(
+            withRecyclerView(
+                R.id.list,
+                itemIndex
+            )
+        ).perform(click())
+
+        intended(
+
+            allOf(
+
+                hasComponent(
+                    HttpCallActivity::class.java.name
+                ),
+
+                hasExtra(
+                    HTTP_CALL_ID,
+                    httpCallId
+                )
+            )
+        )
+    }
+
+    private fun saveHttpCall(
+        url: String,
+        method: String,
+        statusCode: Int,
+        statusText: String,
+        date: Date
+    ): Long {
+
+        val httpCall =
+            HttpCall.Builder()
+
+                .withUrl(url)
+
+                .withMethod(method)
+
+                .withStatusCode(statusCode)
+
+                .withStatusText(statusText)
+
+                .build()
+
+        httpCall.date = date
+
+        return snooperRepo.save(
+            HttpCallRecord.from(httpCall)
+        )
+    }
 }
