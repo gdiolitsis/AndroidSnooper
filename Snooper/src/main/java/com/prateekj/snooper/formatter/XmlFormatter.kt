5c678d7ff6b74dc342bb522fa8973f957dbd685e
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:44:31 +0300

    Update XmlFormatter.kt

--- Snooper/src/main/java/com/prateekj/snooper/formatter/XmlFormatter.kt
+++ Snooper/src/main/java/com/prateekj/snooper/formatter/XmlFormatter.kt
@@ -1,41 +1,87 @@
 package com.prateekj.snooper.formatter
 
 import com.prateekj.snooper.utils.Logger
-
 import org.xml.sax.InputSource
-
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
-
+import java.nio.charset.StandardCharsets
 import javax.xml.transform.OutputKeys
-import javax.xml.transform.Source
-import javax.xml.transform.Transformer
 import javax.xml.transform.sax.SAXSource
 import javax.xml.transform.sax.SAXTransformerFactory
 import javax.xml.transform.stream.StreamResult
 
 class XmlFormatter : ResponseFormatter {
 
-  override fun format(response: String): String {
-    return try {
-      val serializer = SAXTransformerFactory.newInstance().newTransformer().apply {
-        setOutputProperty(OutputKeys.INDENT, "yes")
-        setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes")
-        setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2")
-      }
-      val xmlSource = SAXSource(InputSource(ByteArrayInputStream(response.toByteArray())))
-      val res = StreamResult(ByteArrayOutputStream())
-      serializer.transform(xmlSource, res)
-      String((res.outputStream as ByteArrayOutputStream).toByteArray()).trim { it <= ' ' }
-    } catch (e: Exception) {
-      e.printStackTrace()
-      Logger.e(TAG, e.message, e)
-      response
+    override fun format(
+        response: String
+    ): String {
+
+        return try {
+
+            val transformer =
+                SAXTransformerFactory
+                    .newInstance()
+                    .newTransformer()
+                    .apply {
+
+                        setOutputProperty(
+                            OutputKeys.INDENT,
+                            "yes"
+                        )
+
+                        setOutputProperty(
+                            OutputKeys.OMIT_XML_DECLARATION,
+                            "yes"
+                        )
+
+                        setOutputProperty(
+                            "{http://xml.apache.org/xslt}indent-amount",
+                            "2"
+                        )
+                    }
+
+            val inputStream =
+                ByteArrayInputStream(
+                    response.toByteArray(
+                        StandardCharsets.UTF_8
+                    )
+                )
+
+            val outputStream =
+                ByteArrayOutputStream()
+
+            val xmlSource =
+                SAXSource(
+                    InputSource(inputStream)
+                )
+
+            val result =
+                StreamResult(outputStream)
+
+            transformer.transform(
+                xmlSource,
+                result
+            )
+
+            outputStream.toString(
+                StandardCharsets.UTF_8.name()
+            ).trim()
+
+        } catch (e: Exception) {
+
+            Logger.e(
+                TAG,
+                e.message,
+                e
+            )
+
+            response
+        }
     }
 
-  }
+    companion object {
 
-  companion object {
-    val TAG = XmlFormatter::class.java.simpleName
-  }
+        private val TAG =
+            XmlFormatter::class.java.simpleName
+    }
 }
