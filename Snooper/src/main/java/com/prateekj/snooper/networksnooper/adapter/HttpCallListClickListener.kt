9d774799938eb43f4c3550458681cd134a9b7d66
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 01:45:38 +0300

    Update HttpCallListClickListener.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/adapter/HttpCallListClickListener.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/adapter/HttpCallListClickListener.kt
@@ -2,6 +2,9 @@ package com.prateekj.snooper.networksnooper.adapter
 
 import com.prateekj.snooper.networksnooper.model.HttpCallRecord
 
-interface HttpCallListClickListener {
-    fun onClick(httpCall: HttpCallRecord)
+fun interface HttpCallListClickListener {
+
+    fun onClick(
+        httpCall: HttpCallRecord
+    )
 }
