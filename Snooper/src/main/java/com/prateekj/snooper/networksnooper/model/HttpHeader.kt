87ffed07818082b841e5399fc3adcdc9ddaa18e2
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:42:42 +0300

    Update HttpHeader.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpHeader.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpHeader.kt
@@ -2,26 +2,43 @@ package com.prateekj.snooper.networksnooper.model
 
 class HttpHeader {
 
-  var id: Int = 0
-  var name: String? = null
-  var values: List<HttpHeaderValue> = listOf()
+    var id: Int = 0
 
-  constructor()
+    var name: String? = null
 
-  constructor(name: String) {
-    this.name = name
-  }
+    var values:
+            List<HttpHeaderValue> =
+        emptyList()
 
-  companion object {
+    constructor()
 
-    const val CONTENT_TYPE = "Content-Type"
-
-    fun from(headers: Map<String, List<String>>): List<HttpHeader> {
-      return headers.entries.map { entry ->
-        HttpHeader(entry.key).also {
-          it.values = HttpHeaderValue.from(entry.value)
-        }
-      }
+    constructor(
+        name: String
+    ) {
+        this.name = name
     }
-  }
+
+    companion object {
+
+        const val CONTENT_TYPE =
+            "Content-Type"
+
+        fun from(
+            headers: Map<String, List<String>>
+        ): List<HttpHeader> {
+
+            return headers.entries.map { entry ->
+
+                HttpHeader(
+                    entry.key
+                ).apply {
+
+                    values =
+                        HttpHeaderValue.from(
+                            entry.value
+                        )
+                }
+            }
+        }
+    }
 }
