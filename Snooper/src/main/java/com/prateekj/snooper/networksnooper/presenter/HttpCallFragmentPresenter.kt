73bf5935df54aa8e3ed5afec5f8c1cc175c0674b
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:44:35 +0300

    Update HttpCallFragmentPresenter.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/presenter/HttpCallFragmentPresenter.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/presenter/HttpCallFragmentPresenter.kt
@@ -1,6 +1,5 @@
 package com.prateekj.snooper.networksnooper.presenter
 
-
 import com.prateekj.snooper.formatter.PlainTextFormatter
 import com.prateekj.snooper.formatter.ResponseFormatter
 import com.prateekj.snooper.formatter.ResponseFormatterFactory
@@ -15,70 +14,177 @@ import com.prateekj.snooper.networksnooper.model.HttpHeader
 import com.prateekj.snooper.networksnooper.model.HttpHeader.Companion.CONTENT_TYPE
 import com.prateekj.snooper.networksnooper.viewmodel.HttpBodyViewModel
 import com.prateekj.snooper.networksnooper.views.HttpCallBodyView
-import java.util.ArrayList
+import java.util.Locale
 
 class HttpCallFragmentPresenter(
-  private val repo: SnooperRepo,
-  private val httpCallId: Long,
-  private val httpCallBodyView: HttpCallBodyView,
-  private val formatterFactory: ResponseFormatterFactory,
-  private val executor: BackgroundTaskExecutor
+    private val repo: SnooperRepo,
+    private val httpCallId: Long,
+    private val httpCallBodyView: HttpCallBodyView,
+    private val formatterFactory: ResponseFormatterFactory,
+    private val executor: BackgroundTaskExecutor
 ) {
-  private var mode: Int = 0
-  private var formattedBodyLowerCase: String? = null
 
-  fun init(viewModel: HttpBodyViewModel, mode: Int) {
-    this.mode = mode
-    val httpCallRecord = this.repo.findById(httpCallId)
-    val formatter = getFormatter(httpCallRecord)
-    val bodyToFormat = getBodyToFormat(httpCallRecord)
-    executor.execute(object : BackgroundTask<String> {
-      override fun onExecute(): String {
-        val formattedBody = formatter.format(bodyToFormat ?: "")
-        formattedBodyLowerCase = formattedBody.toLowerCase()
-        return formattedBody
-      }
+    private var mode: Int = 0
 
-      override fun onResult(result: String) {
-        viewModel.init(result)
-        httpCallBodyView.onFormattingDone()
-      }
-    })
-  }
+    private var formattedBodyLowerCase:
+            String = ""
 
-  fun searchInBody(pattern: String) {
-    httpCallBodyView.removeOldHighlightedSpans()
-    if (pattern.isEmpty()) {
-      return
+    fun init(
+        viewModel: HttpBodyViewModel,
+        mode: Int
+    ) {
+
+        this.mode = mode
+
+        val httpCallRecord =
+            repo.findById(httpCallId)
+
+        val formatter =
+            getFormatter(httpCallRecord)
+
+        val bodyToFormat =
+            getBodyToFormat(httpCallRecord)
+                ?: ""
+
+        executor.execute(
+            object : BackgroundTask<String> {
+
+                override fun onExecute():
+                        String {
+
+                    val formattedBody =
+                        formatter.format(
+                            bodyToFormat
+                        )
+
+                    formattedBodyLowerCase =
+                        formattedBody.lowercase(
+                            Locale.getDefault()
+                        )
+
+                    return formattedBody
+                }
+
+                override fun onResult(
+                    result: String
+                ) {
+
+                    viewModel.init(result)
+
+                    httpCallBodyView
+                        .onFormattingDone()
+                }
+            }
+        )
     }
-    val bounds = ArrayList<Bound>()
-    var indexOfKeyword = formattedBodyLowerCase!!.indexOf(pattern)
-    while (indexOfKeyword > -1) {
-      val rightBound = indexOfKeyword + pattern.length
-      bounds.add(Bound(indexOfKeyword, rightBound))
-      indexOfKeyword = formattedBodyLowerCase!!.indexOf(pattern, rightBound)
-    }
-    if (bounds.isNotEmpty()) {
-      httpCallBodyView.highlightBounds(bounds)
-    }
-  }
 
-  private fun getBodyToFormat(httpCallRecord: HttpCallRecord): String? {
-    if (this.mode == ERROR_MODE) {
-      return httpCallRecord.error
+    fun searchInBody(
+        pattern: String
+    ) {
+
+        httpCallBodyView
+            .removeOldHighlightedSpans()
+
+        if (
+            pattern.isEmpty()
+        ) {
+            return
+        }
+
+        val bounds =
+            mutableListOf<Bound>()
+
+        var indexOfKeyword =
+            formattedBodyLowerCase.indexOf(
+                pattern
+            )
+
+        while (indexOfKeyword > -1) {
+
+            val rightBound =
+                indexOfKeyword +
+                        pattern.length
+
+            bounds.add(
+                Bound(
+                    indexOfKeyword,
+                    rightBound
+                )
+            )
+
+            indexOfKeyword =
+                formattedBodyLowerCase.indexOf(
+                    pattern,
+                    rightBound
+                )
+        }
+
+        if (
+            bounds.isNotEmpty()
+        ) {
+
+            httpCallBodyView
+                .highlightBounds(bounds)
+        }
     }
-    return if (this.mode == REQUEST_MODE) httpCallRecord.payload else httpCallRecord.responseBody
-  }
 
-  private fun getFormatter(httpCallRecord: HttpCallRecord): ResponseFormatter {
-    val contentTypeHeader = getContentTypeHeader(httpCallRecord) ?: return PlainTextFormatter()
-    val headerValue = contentTypeHeader.values[0]
-    return this.formatterFactory.getFor(headerValue.value)
-  }
+    private fun getBodyToFormat(
+        httpCallRecord: HttpCallRecord
+    ): String? {
 
-  private fun getContentTypeHeader(httpCall: HttpCallRecord): HttpHeader? {
-    return if (this.mode == REQUEST_MODE) {
-      httpCall.getRequestHeader(CONTENT_TYPE)
-    } else httpCall.getResponseHeader(CONTENT_TYPE)
-  }
+        return when (mode) {
+
+            ERROR_MODE -> {
+                httpCallRecord.error
+            }
+
+            REQUEST_MODE -> {
+                httpCallRecord.payload
+            }
+
+            else -> {
+                httpCallRecord.responseBody
+            }
+        }
+    }
+
+    private fun getFormatter(
+        httpCallRecord: HttpCallRecord
+    ): ResponseFormatter {
+
+        val contentTypeHeader =
+            getContentTypeHeader(
+                httpCallRecord
+            )
+                ?: return PlainTextFormatter()
+
+        val headerValue =
+            contentTypeHeader.values
+                .firstOrNull()
+                ?: return PlainTextFormatter()
+
+        return formatterFactory.getFor(
+            headerValue.value
+        )
+    }
+
+    private fun getContentTypeHeader(
+        httpCall: HttpCallRecord
+    ): HttpHeader? {
+
+        return if (
+            mode == REQUEST_MODE
+        ) {
+
+            httpCall.getRequestHeader(
+                CONTENT_TYPE
+            )
+
+        } else {
+
+            httpCall.getResponseHeader(
+                CONTENT_TYPE
+            )
+        }
+    }
 }
