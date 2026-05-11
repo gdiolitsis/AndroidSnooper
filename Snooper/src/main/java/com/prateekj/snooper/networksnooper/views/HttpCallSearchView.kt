d0bfa2b5a68715462b210fbdd2be25663279efd6
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 02:09:50 +0300

    Update HttpCallSearchView.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/views/HttpCallSearchView.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/views/HttpCallSearchView.kt
@@ -3,9 +3,18 @@ package com.prateekj.snooper.networksnooper.views
 import com.prateekj.snooper.networksnooper.model.HttpCallRecord
 
 interface HttpCallSearchView {
-  fun showResults(httpCallRecords: List<HttpCallRecord>)
-  fun showNoResultsFoundMessage(keyword: String)
-  fun hideSearchResultsView()
-  fun showLoader()
-  fun hideLoader()
+
+    fun showResults(
+        httpCallRecords: List<HttpCallRecord>
+    )
+
+    fun showNoResultsFoundMessage(
+        keyword: String
+    )
+
+    fun hideSearchResultsView()
+
+    fun showLoader()
+
+    fun hideLoader()
 }
