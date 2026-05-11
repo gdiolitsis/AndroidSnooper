7be0d19cbe26ccafadfa9c455560eb7604469b01
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:33:40 +0300

    Update DatabaseAdapter.kt

--- Snooper/src/main/java/com/prateekj/snooper/dbreader/adapter/DatabaseAdapter.kt
+++ Snooper/src/main/java/com/prateekj/snooper/dbreader/adapter/DatabaseAdapter.kt
@@ -9,33 +9,63 @@ import com.prateekj.snooper.dbreader.model.Database
 import kotlinx.android.synthetic.main.db_card_item.view.*
 
 class DatabaseAdapter(
-  private val databaseList: List<Database>,
-  private val dbEventListener: DbEventListener
+    private val databaseList: List<Database>,
+    private val dbEventListener: DbEventListener
 ) : RecyclerView.Adapter<DatabaseAdapter.DbViewHolder>() {
 
-  class DbViewHolder(
-    val view: View,
-    private val dbEventListener: DbEventListener
-  ) : RecyclerView.ViewHolder(view) {
+    class DbViewHolder(
+        private val view: View,
+        private val dbEventListener: DbEventListener
+    ) : RecyclerView.ViewHolder(view) {
 
-    fun bind(db: Database) {
-      view.name.text = db.name
-      view.path.text = db.path
-      this.itemView.setOnClickListener { dbEventListener.onDatabaseClick(db) }
+        fun bind(
+            db: Database
+        ) {
+
+            view.name?.text =
+                db.name ?: ""
+
+            view.path?.text =
+                db.path ?: ""
+
+            itemView.setOnClickListener {
+
+                dbEventListener.onDatabaseClick(db)
+            }
+        }
     }
-  }
 
-  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DbViewHolder {
-    val itemView = LayoutInflater.from(parent.context)
-      .inflate(R.layout.db_card_item, parent, false)
-    return DbViewHolder(itemView, dbEventListener)
-  }
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): DbViewHolder {
 
-  override fun onBindViewHolder(holder: DbViewHolder, position: Int) {
-    holder.bind(databaseList[position])
-  }
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(
+                    R.layout.db_card_item,
+                    parent,
+                    false
+                )
 
-  override fun getItemCount(): Int {
-    return databaseList.size
-  }
+        return DbViewHolder(
+            itemView,
+            dbEventListener
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: DbViewHolder,
+        position: Int
+    ) {
+
+        holder.bind(
+            databaseList[position]
+        )
+    }
+
+    override fun getItemCount(): Int {
+
+        return databaseList.size
+    }
 }
