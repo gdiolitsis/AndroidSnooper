964bfd75f55f7a794689b4637eb73a499f514044
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:19:41 +0300

    Update AdapterDataAppendObserver.kt

--- Snooper/src/main/java/com/prateekj/snooper/customviews/AdapterDataAppendObserver.kt
+++ Snooper/src/main/java/com/prateekj/snooper/customviews/AdapterDataAppendObserver.kt
@@ -2,11 +2,30 @@ package com.prateekj.snooper.customviews
 
 import androidx.recyclerview.widget.RecyclerView
 
-class AdapterDataAppendObserver(private val listener: PageAddedListener) :
-  RecyclerView.AdapterDataObserver() {
+class AdapterDataAppendObserver(
+    private val listener: PageAddedListener
+) : RecyclerView.AdapterDataObserver() {
 
-  override fun onChanged() {
-    super.onChanged()
-    listener.onPageAdded()
-  }
+    override fun onChanged() {
+
+        super.onChanged()
+
+        listener.onPageAdded()
+    }
+
+    override fun onItemRangeInserted(
+        positionStart: Int,
+        itemCount: Int
+    ) {
+
+        super.onItemRangeInserted(
+            positionStart,
+            itemCount
+        )
+
+        if (itemCount > 0) {
+
+            listener.onPageAdded()
+        }
+    }
 }
