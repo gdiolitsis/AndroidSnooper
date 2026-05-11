14bd01489fa34444992e6c3916b35c32ae6a5901
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 01:59:15 +0300

    Update HttpCall.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpCall.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpCall.kt
@@ -2,60 +2,112 @@ package com.prateekj.snooper.networksnooper.model
 
 import java.util.Date
 
-class HttpCall {
+data class HttpCall(
 
-  var url: String? = null
-  var payload: String? = null
-  var method: String? = null
-  var responseBody: String? = null
-  var statusText: String? = null
-  var statusCode: Int = 0
-  var date: Date = Date()
-  var requestHeaders = mapOf<String, List<String>>()
-  var responseHeaders = mapOf<String, List<String>>()
-  var error: String? = null
+    var url: String? = null,
 
-  class Builder {
-    private val httpCall: HttpCall = HttpCall()
+    var payload: String? = null,
 
-    fun withMethod(httpMethod: String) = apply {
-      httpCall.method = httpMethod
+    var method: String? = null,
+
+    var responseBody: String? = null,
+
+    var statusText: String? = null,
+
+    var statusCode: Int = 0,
+
+    var date: Date = Date(),
+
+    var requestHeaders:
+            Map<String, List<String>> = emptyMap(),
+
+    var responseHeaders:
+            Map<String, List<String>> = emptyMap(),
+
+    var error: String? = null
+) {
+
+    class Builder {
+
+        private val httpCall =
+            HttpCall()
+
+        fun withMethod(
+            httpMethod: String
+        ) = apply {
+
+            httpCall.method =
+                httpMethod
+        }
+
+        fun withUrl(
+            url: String
+        ) = apply {
+
+            httpCall.url =
+                url
+        }
+
+        fun withPayload(
+            payload: String
+        ) = apply {
+
+            httpCall.payload =
+                payload
+        }
+
+        fun withResponseBody(
+            responseBody: String
+        ) = apply {
+
+            httpCall.responseBody =
+                responseBody
+        }
+
+        fun withStatusText(
+            statusText: String
+        ) = apply {
+
+            httpCall.statusText =
+                statusText
+        }
+
+        fun withStatusCode(
+            rawStatusCode: Int
+        ) = apply {
+
+            httpCall.statusCode =
+                rawStatusCode
+        }
+
+        fun withRequestHeaders(
+            headers: Map<String, List<String>>
+        ) = apply {
+
+            httpCall.requestHeaders =
+                headers
+        }
+
+        fun withResponseHeaders(
+            headers: Map<String, List<String>>
+        ) = apply {
+
+            httpCall.responseHeaders =
+                headers
+        }
+
+        fun withError(
+            error: String
+        ) = apply {
+
+            httpCall.error =
+                error
+        }
+
+        fun build():
+                HttpCall {
+
+            return httpCall.copy()
+        }
     }
-
-    fun withUrl(url: String) = apply {
-      httpCall.url = url
-    }
-
-    fun build(): HttpCall {
-      return httpCall
-    }
-
-    fun withPayload(payload: String) = apply {
-      httpCall.payload = payload
-    }
-
-    fun withResponseBody(responseBody: String) = apply {
-      httpCall.responseBody = responseBody
-    }
-
-    fun withStatusText(statusText: String) = apply {
-      httpCall.statusText = statusText
-    }
-
-    fun withStatusCode(rawStatusCode: Int) = apply {
-      httpCall.statusCode = rawStatusCode
-    }
-
-    fun withRequestHeaders(headers: Map<String, List<String>>) = apply {
-      httpCall.requestHeaders = headers
-    }
-
-    fun withResponseHeaders(headers: Map<String, List<String>>) = apply {
-      httpCall.responseHeaders = headers
-    }
-
-    fun withError(error: String) = apply {
-      httpCall.error = error
-    }
-  }
 }
