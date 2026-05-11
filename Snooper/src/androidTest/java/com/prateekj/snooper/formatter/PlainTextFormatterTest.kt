fbf86bc78de873a673747954583cf7576a07786e
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 11:56:19 +0300

    Update PlainTextFormatterTest.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/formatter/PlainTextFormatterTest.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/formatter/PlainTextFormatterTest.kt
@@ -1,22 +1,38 @@
 package com.prateekj.snooper.formatter
 
-import org.junit.Test
-
 import com.prateekj.snooper.utils.TestUtilities.readFrom
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
+import org.junit.Test
 
 class PlainTextFormatterTest {
 
-  @Test
-  @Throws(Exception::class)
-  fun shouldReturnFormattedPlainText() {
-    val formatter = PlainTextFormatter()
-    val rawString =
-      readFrom("person_details_formatted_response.txt").replace("\n".toRegex(), "\r")
-    val formattedResponse = formatter.format(rawString)
-    val expectedResponse = readFrom("person_details_formatted_response.txt")
+    @Test
+    @Throws(Exception::class)
+    fun shouldReturnFormattedPlainText() {
 
-    assertThat(formattedResponse, `is`<String>(expectedResponse))
-  }
+        val formatter =
+            PlainTextFormatter()
+
+        val rawString =
+            readFrom(
+                "person_details_formatted_response.txt"
+            ).replace(
+                "\n",
+                "\r"
+            )
+
+        val formattedResponse =
+            formatter.format(rawString)
+
+        val expectedResponse =
+            readFrom(
+                "person_details_formatted_response.txt"
+            )
+
+        assertThat(
+            formattedResponse,
+            `is`(expectedResponse)
+        )
+    }
 }
