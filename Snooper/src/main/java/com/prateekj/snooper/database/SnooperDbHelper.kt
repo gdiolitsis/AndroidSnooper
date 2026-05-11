2e46eb0bf12a75063db472e3e37f37f7230b7542
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 12:26:00 +0300

    Update SnooperDbHelper.kt

--- Snooper/src/main/java/com/prateekj/snooper/database/SnooperDbHelper.kt
+++ Snooper/src/main/java/com/prateekj/snooper/database/SnooperDbHelper.kt
@@ -5,52 +5,113 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.annotation.AnyThread
 import androidx.annotation.MainThread
-
 import com.prateekj.snooper.networksnooper.database.HttpCallRecordContract.Companion.HEADER_CREATE_TABLE
 import com.prateekj.snooper.networksnooper.database.HttpCallRecordContract.Companion.HEADER_VALUE_CREATE_TABLE
 import com.prateekj.snooper.networksnooper.database.HttpCallRecordContract.Companion.HTTP_CALL_RECORD_CREATE_TABLE
 
-class SnooperDbHelper private constructor(context: Context) :
-  SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class SnooperDbHelper private constructor(
+    context: Context
+) : SQLiteOpenHelper(
+    context.applicationContext,
+    DATABASE_NAME,
+    null,
+    DATABASE_VERSION
+) {
 
-  init {
-    super.setWriteAheadLoggingEnabled(true)
-  }
+    init {
 
-  override fun onConfigure(db: SQLiteDatabase) {
-    db.execSQL("PRAGMA foreign_keys = ON")
-  }
-
-  override fun onCreate(db: SQLiteDatabase) {
-    db.execSQL(HTTP_CALL_RECORD_CREATE_TABLE)
-    db.execSQL(HEADER_CREATE_TABLE)
-    db.execSQL(HEADER_VALUE_CREATE_TABLE)
-  }
-
-  override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {}
-
-  override fun setWriteAheadLoggingEnabled(enabled: Boolean) {
-    if (!enabled) {
-      throw UnsupportedOperationException("Write ahead logging is required.")
-    }
-  }
-
-  companion object {
-
-    private const val DATABASE_VERSION = 1
-    const val DATABASE_NAME = "snooper.db"
-    private var INSTANCE: SnooperDbHelper? = null
-
-    @MainThread
-    fun getInstance(context: Context): SnooperDbHelper {
-      return INSTANCE ?: SnooperDbHelper(context).also {
-        INSTANCE = it
-      }
+        super.setWriteAheadLoggingEnabled(true)
     }
 
-    @AnyThread
-    fun create(context: Context): SnooperDbHelper {
-      return SnooperDbHelper(context)
+    override fun onConfigure(
+        db: SQLiteDatabase
+    ) {
+
+        super.onConfigure(db)
+
+        db.execSQL(
+            "PRAGMA foreign_keys = ON"
+        )
     }
-  }
+
+    override fun onCreate(
+        db: SQLiteDatabase
+    ) {
+
+        db.execSQL(
+            HTTP_CALL_RECORD_CREATE_TABLE
+        )
+
+        db.execSQL(
+            HEADER_CREATE_TABLE
+        )
+
+        db.execSQL(
+            HEADER_VALUE_CREATE_TABLE
+        )
+    }
+
+    override fun onUpgrade(
+        db: SQLiteDatabase,
+        oldVersion: Int,
+        newVersion: Int
+    ) {
+
+        // no-op
+    }
+
+    override fun setWriteAheadLoggingEnabled(
+        enabled: Boolean
+    ) {
+
+        if (!enabled) {
+
+            throw UnsupportedOperationException(
+                "Write ahead logging is required."
+            )
+        }
+
+        super.setWriteAheadLoggingEnabled(
+            true
+        )
+    }
+
+    companion object {
+
+        private const val DATABASE_VERSION =
+            1
+
+        const val DATABASE_NAME =
+            "snooper.db"
+
+        @Volatile
+        private var INSTANCE:
+                SnooperDbHelper? = null
+
+        @MainThread
+        fun getInstance(
+            context: Context
+        ): SnooperDbHelper {
+
+            return INSTANCE ?: synchronized(this) {
+
+                INSTANCE ?: SnooperDbHelper(
+                    context.applicationContext
+                ).also {
+
+                    INSTANCE = it
+                }
+            }
+        }
+
+        @AnyThread
+        fun create(
+            context: Context
+        ): SnooperDbHelper {
+
+            return SnooperDbHelper(
+                context.applicationContext
+            )
+        }
+    }
 }
