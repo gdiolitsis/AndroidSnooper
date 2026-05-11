b0e427e4ea5954c389408b6142a56c1b41da6637
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 01:23:51 +0300

    Update DbEventListener.kt

--- Snooper/src/main/java/com/prateekj/snooper/dbreader/adapter/DbEventListener.kt
+++ Snooper/src/main/java/com/prateekj/snooper/dbreader/adapter/DbEventListener.kt
@@ -2,6 +2,9 @@ package com.prateekj.snooper.dbreader.adapter
 
 import com.prateekj.snooper.dbreader.model.Database
 
-interface DbEventListener {
-  fun onDatabaseClick(db: Database)
+fun interface DbEventListener {
+
+    fun onDatabaseClick(
+        db: Database
+    )
 }
