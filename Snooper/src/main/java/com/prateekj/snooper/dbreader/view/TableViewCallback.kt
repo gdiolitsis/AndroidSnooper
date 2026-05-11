9d211462dd4aba76fa776218c950a390769b0d7b
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 01:30:25 +0300

    Update TableViewCallback.kt

--- Snooper/src/main/java/com/prateekj/snooper/dbreader/view/TableViewCallback.kt
+++ Snooper/src/main/java/com/prateekj/snooper/dbreader/view/TableViewCallback.kt
@@ -1,9 +1,12 @@
 package com.prateekj.snooper.dbreader.view
 
-
 import com.prateekj.snooper.dbreader.model.Table
 
 interface TableViewCallback {
-  fun onTableFetchStarted()
-  fun onTableFetchCompleted(table: Table)
+
+    fun onTableFetchStarted()
+
+    fun onTableFetchCompleted(
+        table: Table
+    )
 }
