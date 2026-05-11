3d8203acf52c989d9a9dd121d2baef5e3b1eac11
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 13:43:33 +0300

    Update HttpHeaderValue.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpHeaderValue.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/model/HttpHeaderValue.kt
@@ -1,20 +1,35 @@
 package com.prateekj.snooper.networksnooper.model
 
 class HttpHeaderValue {
-  private var id: Int = 0
-  lateinit var value: String
 
-  constructor() {}
+    private var id: Int = 0
 
-  constructor(value: String) {
-    this.value = value
-  }
+    lateinit var value: String
 
-  fun setId(id: Int) {
-    this.id = id
-  }
+    constructor()
 
-  companion object {
-    fun from(strings: List<String>) = strings.map { HttpHeaderValue(it) }
-  }
+    constructor(
+        value: String
+    ) {
+        this.value = value
+    }
+
+    fun setId(
+        id: Int
+    ) {
+        this.id = id
+    }
+
+    companion object {
+
+        fun from(
+            strings: List<String>
+        ): List<HttpHeaderValue> {
+
+            return strings.map {
+
+                HttpHeaderValue(it)
+            }
+        }
+    }
 }
