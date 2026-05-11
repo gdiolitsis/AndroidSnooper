d9f9dd252064dd1f56f4307a05f8c755bed13c7d
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 11:47:26 +0300

    Update TestApplication.kt

--- Snooper/src/androidTest/java/com/prateekj/snooper/TestApplication.kt
+++ Snooper/src/androidTest/java/com/prateekj/snooper/TestApplication.kt
@@ -3,8 +3,11 @@ package com.prateekj.snooper
 import android.app.Application
 
 class TestApplication : Application() {
-  override fun onCreate() {
-    super.onCreate()
-    AndroidSnooper.init(this)
-  }
+
+    override fun onCreate() {
+
+        super.onCreate()
+
+        AndroidSnooper.init(this)
+    }
 }
