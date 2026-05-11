bb61b4e89f6f8c285277b3a7cebb29d878a37884
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-11 11:39:59 +0300

    Update SpringHttpRequestTransformer.kt

--- Snooper-Spring/src/main/java/org/springframework/http/client/SpringHttpRequestTransformer.kt
+++ Snooper-Spring/src/main/java/org/springframework/http/client/SpringHttpRequestTransformer.kt
@@ -3,35 +3,116 @@ package org.springframework.http.client
 import com.prateekj.snooper.networksnooper.model.HttpCall
 import org.springframework.http.HttpRequest
 import java.io.IOException
+import java.nio.charset.Charset
 
 class SpringHttpRequestTransformer {
 
-  @Throws(IOException::class)
-  fun transform(
-    httpRequest: HttpRequest,
-    requestPayload: ByteArray,
-    httpResponse: ClientHttpResponse
-  ): HttpCall {
-    return HttpCall.Builder()
-      .withUrl(httpRequest.uri.toString())
-      .withPayload(String(requestPayload))
-      .withMethod(httpRequest.method.toString())
-      .withResponseBody(httpResponse.body.bufferedReader().use { it.readText() })
-      .withStatusCode(httpResponse.rawStatusCode)
-      .withStatusText(httpResponse.statusCode.reasonPhrase)
-      .withRequestHeaders(httpRequest.headers)
-      .withResponseHeaders(httpResponse.headers)
-      .build()
-  }
+    @Throws(IOException::class)
+    fun transform(
+        httpRequest: HttpRequest,
+        requestPayload: ByteArray,
+        httpResponse: ClientHttpResponse
+    ): HttpCall {
 
-  @Throws(IOException::class)
-  fun transform(httpRequest: HttpRequest, requestPayload: ByteArray, e: Exception): HttpCall {
-    return HttpCall.Builder()
-      .withUrl(httpRequest.uri.toString())
-      .withPayload(String(requestPayload))
-      .withMethod(httpRequest.method.toString())
-      .withRequestHeaders(httpRequest.headers)
-      .withError(e.toString())
-      .build()
-  }
+        val payload = try {
+
+            String(
+                requestPayload,
+                Charset.forName("UTF-8")
+            )
+
+        } catch (_: Throwable) {
+            ""
+        }
+
+        val responseBody = try {
+
+            httpResponse.body
+                ?.bufferedReader()
+                ?.use { it.readText() }
+                ?: ""
+
+        } catch (_: Throwable) {
+            ""
+        }
+
+        return HttpCall.Builder()
+
+            .withUrl(
+                httpRequest.uri.toString()
+            )
+
+            .withPayload(
+                payload
+            )
+
+            .withMethod(
+                httpRequest.method.toString()
+            )
+
+            .withResponseBody(
+                responseBody
+            )
+
+            .withStatusCode(
+                httpResponse.rawStatusCode
+            )
+
+            .withStatusText(
+                httpResponse.statusCode.reasonPhrase
+            )
+
+            .withRequestHeaders(
+                httpRequest.headers
+            )
+
+            .withResponseHeaders(
+                httpResponse.headers
+            )
+
+            .build()
+    }
+
+    @Throws(IOException::class)
+    fun transform(
+        httpRequest: HttpRequest,
+        requestPayload: ByteArray,
+        e: Exception
+    ): HttpCall {
+
+        val payload = try {
+
+            String(
+                requestPayload,
+                Charset.forName("UTF-8")
+            )
+
+        } catch (_: Throwable) {
+            ""
+        }
+
+        return HttpCall.Builder()
+
+            .withUrl(
+                httpRequest.uri.toString()
+            )
+
+            .withPayload(
+                payload
+            )
+
+            .withMethod(
+                httpRequest.method.toString()
+            )
+
+            .withRequestHeaders(
+                httpRequest.headers
+            )
+
+            .withError(
+                e.toString()
+            )
+
+            .build()
+    }
 }
