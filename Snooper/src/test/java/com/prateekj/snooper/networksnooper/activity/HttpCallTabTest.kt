9f69670298a0aa455579056973ce3595e4c55490
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 02:18:41 +0300

    Update HttpCallTabTest.kt

--- Snooper/src/test/java/com/prateekj/snooper/networksnooper/activity/HttpCallTabTest.kt
+++ Snooper/src/test/java/com/prateekj/snooper/networksnooper/activity/HttpCallTabTest.kt
@@ -1,28 +1,49 @@
 package com.prateekj.snooper.networksnooper.activity
 
 import com.prateekj.snooper.R
-
-import org.junit.Test
-
+import com.prateekj.snooper.networksnooper.activity.HttpCallTab.ERROR
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab.HEADERS
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab.REQUEST
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab.RESPONSE
 import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.assertThat
+import org.junit.Test
 
 class HttpCallTabTest {
+
     @Test
     fun shouldReturnResponseTabDetails() {
-        assertThat(RESPONSE.tabTitle, `is`(R.string.response))
+
+        assertThat(
+            RESPONSE.tabTitle,
+            `is`(R.string.response)
+        )
     }
 
     @Test
     fun shouldReturnRequestTabDetails() {
-        assertThat(REQUEST.tabTitle, `is`(R.string.request))
+
+        assertThat(
+            REQUEST.tabTitle,
+            `is`(R.string.request)
+        )
     }
 
     @Test
     fun shouldReturnHeadersTabDetails() {
-        assertThat(HEADERS.tabTitle, `is`(R.string.headers))
+
+        assertThat(
+            HEADERS.tabTitle,
+            `is`(R.string.headers)
+        )
+    }
+
+    @Test
+    fun shouldReturnErrorTabDetails() {
+
+        assertThat(
+            ERROR.tabTitle,
+            `is`(R.string.error)
+        )
     }
 }
