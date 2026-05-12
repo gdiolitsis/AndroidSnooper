b6892138c6349132980b9300eb6036a762b52781
gdiolitsis <48429745+gdiolitsis@users.noreply.github.com>
2026-05-12 03:13:06 +0300

    Update HttpCallRenderer.kt

--- Snooper/src/main/java/com/prateekj/snooper/networksnooper/helper/HttpCallRenderer.kt
+++ Snooper/src/main/java/com/prateekj/snooper/networksnooper/helper/HttpCallRenderer.kt
@@ -1,83 +1,64 @@
 package com.prateekj.snooper.networksnooper.helper
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab.ERROR
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab.HEADERS
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab.REQUEST
 import com.prateekj.snooper.networksnooper.activity.HttpCallTab.RESPONSE
+import com.prateekj.snooper.networksnooper.fragment.HttpCallFragment
+import com.prateekj.snooper.networksnooper.fragment.HttpHeadersFragment
 import com.prateekj.snooper.networksnooper.views.HttpCallView
 
 class HttpCallRenderer(
-    private val httpCallView:
-            HttpCallView,
-    private val hasError:
-            Boolean
+    private val httpCallView: HttpCallView,
+    private val errorMode: Boolean
 ) {
 
-    private val tabs:
-            List<HttpCallTab> by lazy {
-
-        if (hasError) {
-
+    private val tabs: List<HttpCallTab> =
+        if (errorMode) {
             listOf(
                 ERROR,
                 REQUEST,
                 HEADERS
             )
-
         } else {
-
             listOf(
                 RESPONSE,
                 REQUEST,
                 HEADERS
             )
         }
-    }
-
-    fun getTabs():
-            List<HttpCallTab> {
-
-        return tabs
-    }
 
     fun getFragment(
         position: Int
     ): Fragment {
 
-        return when (
-            tabs.getOrNull(position)
-        ) {
-
-            ERROR -> {
-
-                httpCallView
-                    .getExceptionFragment()
-            }
+        return when (tabs[position]) {
 
             RESPONSE -> {
+                httpCallView.getResponseBodyFragment()
+            }
 
-                httpCallView
-                    .getResponseBodyFragment()
+            ERROR -> {
+                httpCallView.getExceptionFragment()
             }
 
             REQUEST -> {
-
-                httpCallView
-                    .getRequestBodyFragment()
+                httpCallView.getRequestBodyFragment()
             }
 
-            HEADERS -> {
-
-                httpCallView
-                    .getHeadersFragment()
-            }
-
-            null -> {
-
-                Fragment()
+            else -> {
+                httpCallView.getHeadersFragment()
             }
         }
+    }
+
+    fun tabs():
+            List<HttpCallTab> {
+
+        return tabs
     }
 }
